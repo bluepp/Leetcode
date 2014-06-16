d5508f37b3901728ba5781734939d6a153edc263
bluepp <bluepp@gmail.com>
2014-06-16 17:34:11 +0800

    Update MinimumPathSum.h

--- MinimumPathSum.h
+++ MinimumPathSum.h
@@ -35,7 +35,7 @@
     
    
    
-/* my solution, not correct, how to?! */   
+/* my solution */   
     
      int minPathSum(vector<vector<int> > &grid) {
         if (grid.empty())
@@ -47,21 +47,18 @@
             for(int j = 0; j < n; j++)
                 dp[i][j] = 0;
                 
-        vector<int> vec(n, 0);
-        grid.push_back(vec);
-       
-        for (int i = 0; i <= m; i++)
-            grid[i].push_back(0);
-            
-                
-                
         dp[0][0] = grid[0][0];
+        
+        for (int i = 1; i < m; i++)
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        for (int j = 1; j < n; j++)
+            dp[0][j] = dp[0][j-1] + grid[0][j];
         
         for (int i = 1; i < m; i++)
         {
             for (int j = 1; j < n; j++)
             {
-                dp[i][j] += min(grid[i+1][j], grid[i][j+1]);
+                dp[i][j] = min(dp[i-1][j],dp[i][j-1])+grid[i][j];
             }
         }
         
