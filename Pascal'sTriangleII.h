2809989ed76fed785d0000aa60880da8f8f0afce
bluepp <bluepp@gmail.com>
2014-06-18 16:36:58 +0800

    Create Pascal'sTriangleII.h

---
+++ Pascal'sTriangleII.h
@@ -0,0 +1,31 @@
+/*
+    bluepp
+    2014-06-18
+    May the force be with me!
+    
+    Problem:    Pascal's Triangle II
+    Source:     https://oj.leetcode.com/problems/pascals-triangle-ii/
+    Notes:
+    Given an index k, return the kth row of the Pascal's triangle.
+    For example, given k = 3,
+    Return [1,3,3,1].
+    Note:
+    Could you optimize your algorithm to use only O(k) extra space?
+
+    Solution: from back to forth...
+ */
+ 
+    vector<int> getRow(int rowIndex) {
+        vector<int> res(rowIndex+1, 1);
+        
+        for (int i = 1; i < rowIndex+1; i++)
+        {
+            for (int j = i-1; j >= 1 ; j--)
+            {
+                res[j] += res[j-1];
+            }
+        }
+        
+        return res;
+        
+    }
