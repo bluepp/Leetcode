1333cec8b0ac77e0f024fa003968fce0a8b84982
bluepp <bluepp@gmail.com>
2014-06-19 16:53:53 +0800

    Create Pow(x,n).h

---
+++ Pow(x,n).h
@@ -0,0 +1,34 @@
+/*
+    bluepp
+    2014-06-19
+    May the force be with me!
+    
+    Problem:    Pow(x, n)
+    Source:     https://oj.leetcode.com/problems/powx-n/
+    Notes:
+    Implement pow(x, n).
+ 
+    Solution: recursion.
+ */
+ 
+    double pow(double x, int n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        bool neg = false;
+        if (n < 0)
+        {
+            neg = true;
+            n = -n;
+        }
+        
+        double r = pow(x, n/2);
+        double res = r * r;
+        if (n % 2)
+            res *= x;
+        if (neg)
+            res = 1 / res;
+        
+        return res;    
+    }
