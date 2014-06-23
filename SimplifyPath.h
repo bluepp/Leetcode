bdceae02d106c63ba16ca524913e99428018e2c0
bluepp <bluepp@gmail.com>
2014-06-23 23:43:17 +0800

    Create SimplifyPath.h

---
+++ SimplifyPath.h
@@ -0,0 +1,48 @@
+/*
+    bluepp
+    2014-06-23
+    May the force be with me!
+    
+     
+ Problem:    Simplify Path
+ Difficulty: Easy
+ Source:     https://oj.leetcode.com/problems/simplify-path/
+ Notes:
+ Given an absolute path for a file (Unix-style), simplify it.
+
+ For example,
+ path = "/home/", => "/home"
+ path = "/a/./b/../../c/", => "/c"
+
+ Corner Cases:
+ Did you consider the case where path = "/../"?
+ In this case, you should return "/".
+ Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
+ In this case, you should ignore redundant slashes and return "/home/foo".
+
+ Solution: Add an additional '/' at the end of 'path' for simply detecting the end.
+ */
+ 
+    string simplifyPath(string path) {
+        string res;
+        path += "/";
+        size_t pos = path.find_first_of("/"), last = 0;
+        while (pos != string::npos)
+        {
+            string s = path.substr(last, pos - last);
+            if (s == "..") 
+            {
+                if (!res.empty())
+                    res.resize(res.find_last_of("/"));
+            }
+            else if (!s.empty() && s != ".")
+            {
+                res += "/";
+                res += s;
+            }
+            last = pos + 1;
+            pos = path.find_first_of("/", last);
+        }
+        return res.empty() ? "/" : res;
+        
+    }
