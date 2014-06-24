4c3bcf40a17e2a1985b778eca355bf35b8d7d9b3
bluepp <bluepp@gmail.com>
2014-06-24 22:58:19 +0800

    Create SingleNumber.h

---
+++ SingleNumber.h
@@ -0,0 +1,26 @@
+/*
+    bluepp
+    2014-06-24
+    May the force be with me!
+    
+   
+ Problem:    Single Number
+ Source:     http://oj.leetcode.com/problems/single-number/
+ Notes:
+ Given an array of integers, every element appears twice except for one. 
+ Find that single one.
+ Your algorithm should have a linear runtime complexity. 
+ Could you implement it without using extra memory?
+
+ Solution: XOR.
+*/
+
+    int singleNumber(int a[], int n) {
+        int x = 0;
+        
+        for (int i = 0; i < n; i++)
+            x ^= a[i];
+        
+        return x;   
+    
+    }
