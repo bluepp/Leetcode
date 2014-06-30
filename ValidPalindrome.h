b7e4d99807be032ede323d896207d01106fa348c
bluepp <bluepp@gmail.com>
2014-07-01 01:35:58 +0800

    Create ValidPalindrome.h

---
+++ ValidPalindrome.h
@@ -0,0 +1,19 @@
+/*
+  bluepp
+  2014-07-01
+  May the force be with me!
+  
+  
+ Problem:    Valid Palindrome
+ Source:     https://oj.leetcode.com/problems/valid-palindrome/
+ Notes:
+ Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+ For example,
+ "A man, a plan, a canal: Panama" is a palindrome.
+ "race a car" is not a palindrome.
+ Note:
+ Have you consider that the string might be empty? This is a good question to ask during an interview.
+ For the purpose of this problem, we define empty string as valid palindrome.
+
+ Solution: traverse from both side.
+ */
