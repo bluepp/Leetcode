2d67de8e63dba22312f66bc15aa510b98e193b9f
bluepp <bluepp@gmail.com>
2014-06-30 15:53:20 +0800

    Create UniqueBinarySearchTrees.h

---
+++ UniqueBinarySearchTrees.h
@@ -0,0 +1,38 @@
+/*
+    bluepp
+    2014-06-30
+    May the force be with me!
+    
+    
+ Problem:    Unique Binary Search Trees
+ Source:     https://oj.leetcode.com/problems/unique-binary-search-trees/
+ Notes:
+ Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+ For example,
+ Given n = 3, there are a total of 5 unique BST's.
+ 1         3     3      2      1
+  \       /     /      / \      \
+   3     2     1      1   3      2
+  /     /       \                 \
+ 2     1         2                 3
+
+ Solution: dp.
+*/
+
+
+    int numTrees(int n) {
+        int dp[n+1];
+        memset(dp, 0, sizeof(dp));
+        dp[0] = 1;
+        
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+        }
+        
+        return dp[n];
+        
+    }
