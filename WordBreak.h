3e5c72954ea82c8458e05d6098ee98fca4158c3a
bluepp <bluepp@gmail.com>
2014-07-02 14:28:38 +0800

    Create WordBreak.h

---
+++ WordBreak.h
@@ -0,0 +1,40 @@
+/*
+    bluepp
+    2014-07-02
+    May the force be with me!
+    
+ Problem:    Word Break
+ Source:     http://oj.leetcode.com/problems/word-break/
+ Notes:
+ Given a string s and a dictionary of words dict, determine if s can be segmented into 
+ a space-separated sequence of one or more dictionary words.
+ For example, given
+ s = "leetcode",
+ dict = ["leet", "code"].
+ Return true because "leetcode" can be segmented as "leet code".
+
+ Solution: dp.
+*/
+
+    bool wordBreak(string s, unordered_set<string> &dict) {
+        int n = s.size();
+        bool dp[n+1];
+        memset(dp, false, sizeof(dp));
+        dp[n] = true;
+        
+        for (int i = n; i >= 0; i--)
+        {
+            for (int j = i+1; j <= n ; j ++)
+            {
+                if (dp[j] && dict.find(s.substr(i, j-i)) != dict.end())
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        
+        return dp[0];
+        
+    }
