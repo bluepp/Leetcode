d48278a2a8ec8d1156d20bf0447a2378d8e114ba
bluepp <bluepp@gmail.com>
2014-07-06 03:03:06 +0800

    Update BestTimetoBuyandSellStock.h

--- BestTimetoBuyandSellStock.h
+++ BestTimetoBuyandSellStock.h
@@ -1,10 +1,11 @@
 /*
     bluepp
     2014-05-31
+    2014-07-06
     May the force be with me!
     
     Problem:    Best Time to Buy and Sell Stock
-    Source:     http://leetcode.com/onlinejudge#question_121
+    Source:     https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock/
     Notes:
     Say you have an array for which the ith element is the price of a given stock on day i.
     If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), 
@@ -28,4 +29,20 @@
         
         return maxprofit;
         
+    }
+
+
+/* 2014-07-06 */
+    int maxProfit(vector<int> &prices) {
+        int n = prices.size();
+        int min_price = INT_MAX, mprofit = 0;
+        
+        for (int i = 0; i < n; i ++)
+        {
+            if (prices[i] < min_price) min_price = prices[i];
+            
+            if (prices[i] - min_price > mprofit) mprofit = prices[i] - min_price;
+        }
+        
+        return mprofit;
     }
