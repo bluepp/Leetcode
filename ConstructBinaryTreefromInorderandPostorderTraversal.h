39844096303bbdeb5068acff4460857492da6960
bluepp <bluepp@gmail.com>
2014-07-08 23:54:37 +0800

    Update ConstructBinaryTreefromInorderandPostorderTraversal.h

--- ConstructBinaryTreefromInorderandPostorderTraversal.h
+++ ConstructBinaryTreefromInorderandPostorderTraversal.h
@@ -26,20 +26,21 @@
  
  
   	
-  	TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+ TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
         int n = inorder.size();
-        return _buildTree(inorder.begin(), postorder.begin(), n);
+        return _build(inorder.begin(), postorder.begin(), n);
     }
     
-    TreeNode * _buildTree(vector<int>::iterator inorder, vector<int>::iterator postorder, int n)
+    TreeNode *_build(vector<int>::iterator inorder, vector<int>::iterator postorder, int n)
     {
-        if (n <= 0) return NULL;
-        vector<int>::iterator it = find(inorder, inorder+n, *(postorder+n-1));
-        int index = it - inorder;
-          
-        TreeNode *root = new TreeNode(*(postorder+n-1));
-        root->left = _buildTree(inorder, postorder, index);
-        root->right = _buildTree(inorder+index+1, postorder+index, n-index-1);
+        if (n == 0) return NULL;
+        int rootval = postorder[n-1];
+        TreeNode *root = new TreeNode(rootval);
         
+        vector<int>::iterator it = find(inorder, inorder+n, rootval);
+        int index = it-inorder;
+        
+        root->left = _build(inorder, postorder, index);
+        root->right = _build(inorder+index+1, postorder+index, n-index-1);
         return root;
     }
