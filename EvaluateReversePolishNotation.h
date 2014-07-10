55512734233f77348251dad7065a635f3767e822
bluepp <bluepp@gmail.com>
2014-07-10 15:46:26 +0800

    Update EvaluateReversePolishNotation.h

--- EvaluateReversePolishNotation.h
+++ EvaluateReversePolishNotation.h
@@ -1,6 +1,7 @@
 /* 
     bluepp
     2014-06-08
+    2014-07-10
     May the force be with me!
     
     Problem:    Evaluate Reverse Polish Notation
@@ -14,30 +15,26 @@
  */
  
     int evalRPN(vector<string> &tokens) {
+        int n = tokens.size();
         stack<int> s;
         
-        for (int i = 0;i < tokens.size(); i ++)
+        for (int i = 0; i < n; i++)
         {
-            if (tokens[i] != "+" && tokens[i] != "-"
-                && tokens[i] != "*" && tokens[i] != "/")
+            string op = tokens[i];
+            if (op != "+" && op != "-" && op != "*" && op != "/")
                 s.push(stoi(tokens[i]));
             else
             {
-                int tmp = 0;
-                int r = s.top(); s.pop();
-                int l = s.top(); s.pop();
+                int n1 = s.top(); s.pop();
+                int n2 = s.top(); s.pop();
+                int n;
                 
-                if (tokens[i] == "+")
-                    tmp = l + r;
-                else if (tokens[i] == "-")
-                    tmp = l-r;
-                else if (tokens[i] == "*")
-                    tmp = l*r;
-                else
-                    tmp = l/r;
-                s.push(tmp);    
+                if (op == "+") n = n1+n2;
+                else if (op == "-") n = n2-n1;
+                else if (op == "*") n = n1*n2;
+                else n = n2/n1;
+                s.push(n);
             }
-                
         }
         
         return s.top();
