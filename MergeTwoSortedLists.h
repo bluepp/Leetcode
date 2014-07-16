03e6bc0917ff79907a06d27abb24eeb1f5e9adab
bluepp <bluepp@gmail.com>
2014-07-16 10:46:36 +0800

    Update MergeTwoSortedLists.h

--- MergeTwoSortedLists.h
+++ MergeTwoSortedLists.h
@@ -1,6 +1,7 @@
 /*
     bluepp
     2014-06-16
+    2014-07-16
     May the force be with me!
     
     Problem:    Merge Two Sorted Lists
@@ -12,31 +13,32 @@
     Solution: ...
  */
  
-    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        ListNode dummy(0);
-        ListNode *l = &dummy;
+   ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+       ListNode dummy(0);
+       ListNode*pCurr = &dummy;
+       ListNode *p1 = l1, *p2 = l2;
+       
+       while (p1 && p2)
+       {
+           int value = 0;
+           if (p1->val < p2->val)
+           {
+               pCurr->next = p1;
+               p1 = p1->next;
+           }
+           else 
+           {
+               pCurr->next = p2;
+               p2 = p2->next;
+           }
+           
+           pCurr = pCurr->next;
+       }
+       
+       if (p1) pCurr->next = p1;
+       if (p2) pCurr->next = p2;
+       
+       return dummy.next;
         
-        while (l1 && l2)
-        {
-            if (l1->val <= l2->val)
-            {
-                l->next = l1;
-                l1 = l1->next;
-            }
-            else
-            {
-                l->next = l2;
-                l2 = l2->next;
-            }
-            
-            l = l->next;
-        }
-        
-        if (l1)
-            l->next = l1;
-        else
-            l->next = l2;
-        
-        return dummy.next;
        
     }
