cb42ad5c32ffd9933f73ebed68ca820e0d99d76a
bluepp <bluepp@gmail.com>
2014-07-31 00:52:04 +0800

    Update UniquePaths.h

--- UniquePaths.h
+++ UniquePaths.h
@@ -31,10 +31,10 @@
         
         dp[m-1][n-1] = 1;
         for (int i = m-2; i >= 0; i--)
-            dp[i][n-1] += dp[i+1][n-1];
+            dp[i][n-1] = dp[i+1][n-1];
         
         for(int j = n-2; j >= 0; j--)
-            dp[m-1][j] += dp[m-1][j+1];
+            dp[m-1][j] = dp[m-1][j+1];
             
         for (int i = m-2; i >= 0; i--)
         {
