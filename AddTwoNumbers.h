13e910a7f01bfde5a17684ec20af2f8ae249f264
bluepp <bluepp@gmail.com>
2014-08-03 16:55:14 +0800

    Update AddTwoNumbers.h

--- AddTwoNumbers.h
+++ AddTwoNumbers.h
@@ -84,3 +84,36 @@
         return dummy.next;    
         
     }
+    
+    
+    /* -----2014-08-03------------------ */
+       ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+       ListNode dummy(0);
+       ListNode *pCurr = &dummy;
+       int carry = 0;
+       
+       while (l1 || l2 || carry)
+       {
+           int sum = carry;
+           if (l1)
+           {
+               sum += l1->val;
+               l1 = l1->next;
+           }
+           if (l2)
+           {
+               sum += l2->val;
+               l2 = l2->next;
+           }
+           
+           carry = sum >= 10 ? 1: 0;
+           sum %= 10;
+           ListNode *pNode = new ListNode(sum);
+           pCurr->next = pNode;
+           pCurr = pCurr->next;
+           
+       }
+       
+       return dummy.next;
+        
+    }
