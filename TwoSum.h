6389900f1916acc395c359f8a461a27a73158107
bluepp <bluepp@gmail.com>
2014-08-25 11:54:25 +0800

    Update TwoSum.h

--- TwoSum.h
+++ TwoSum.h
@@ -10,6 +10,10 @@
  Source:     https://oj.leetcode.com/problems/two-sum/
  Notes:
  Given an array of integers, find two numbers such that they add up to a specific target number.
+ 
+ update, can use extra space, cannot use extra space
+ http://www.mitbbs.com/mitbbs_article_t.php?board=JobHunting&gid=32517841&ftype=0
+ 
 
  The function twoSum should return indices of the two numbers such that they add up to the target, 
  where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
