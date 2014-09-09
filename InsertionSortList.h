92fa2763626f5d075dc23944f36aed19fe81f825
bluepp <bluepp@gmail.com>
2014-09-09 23:41:26 +0800

    Create InsertionSortList.h

---
+++ InsertionSortList.h
@@ -0,0 +1,45 @@
+/*
+  2014-09-09
+  bluepp
+  May the force be with me!
+  
+ Source:     http://oj.leetcode.com/problems/insertion-sort-list/
+ Notes:
+ Sort a linked list using insertion sort.
+
+ Solution: ...
+ */
+ 
+     ListNode *insertionSortList(ListNode *head) {
+        ListNode dummy(INT_MIN);
+        dummy.next = head;
+        ListNode *pCurr = &dummy;
+        
+        while (pCurr->next)
+        {
+            if (pCurr->next->val >= pCurr->val)
+            {
+                pCurr = pCurr->next;
+            }
+            else
+            {
+                insert(&dummy, pCurr, pCurr->next);
+            }
+            
+        }
+        
+        return dummy.next;
+    }
+    
+    void insert(ListNode *head, ListNode *tail, ListNode *pNode)
+    {
+        ListNode *pCurr = head;
+        
+        while (pCurr->next->val < pNode->val)
+            pCurr = pCurr->next;
+        
+        tail->next = pNode->next;
+        pNode->next = pCurr->next;
+        pCurr->next = pNode;
+    }
+  
