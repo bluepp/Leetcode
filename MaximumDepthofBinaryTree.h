23f8db5d3f1f12f44db16980048503a6b90a4274
bluepp <bluepp@gmail.com>
2014-09-09 23:18:27 +0800

    Update MaximumDepthofBinaryTree.h

--- MaximumDepthofBinaryTree.h
+++ MaximumDepthofBinaryTree.h
@@ -1,7 +1,10 @@
 /*
     bluepp
+    
     2014-06-14
     2014-07-16
+    2014-09-09
+    
     May the force be with you!
     
     Problem:    Maximum Depth of Binary Tree
@@ -28,4 +31,37 @@
         int l = maxDepth(root->left);
         int r = maxDepth(root->right);
         return 1 + max(l, r);
+    }
+
+
+/* 2014-09-09, my solution */
+    int maxDepth(TreeNode *root) {
+        if (!root) return 0;
+        if (!root->left && !root->right) return 1;
+        
+        queue<TreeNode *> q;
+        q.push(root);
+        q.push(NULL);
+        int ret = 0;
+        int count = 0;
+        
+        while (!q.empty())
+        {
+            TreeNode *pCurr = q.front(); q.pop();
+            
+            if (pCurr)
+            {
+                if (pCurr->left) q.push(pCurr->left);
+                if (pCurr->right) q.push(pCurr->right);
+            }
+            else
+            {
+                count ++;
+                if (q.empty()) break;
+                q.push(NULL);
+            }
+            
+        }
+        
+        return count;
     }
