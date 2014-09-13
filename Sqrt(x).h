e943da16cd5cbb7c6e3ceeaa2e6f1442742ed4e6
bluepp <bluepp@gmail.com>
2014-09-14 03:38:47 +0800

    Update Sqrt(x).h

--- Sqrt(x).h
+++ Sqrt(x).h
@@ -1,8 +1,11 @@
 /*
     bluepp
+    
     2014-06-26
     2014-07-27
     2014-08-19
+    2014-09-14
+    
     May the force be with me!
     
    
@@ -16,6 +19,28 @@
            2. Newton iteration method. x(i+1) = (x(i) + n/x(i)) / 2.
  See  (http://www.cnblogs.com/AnnieKim/archive/2013/04/18/3028607.html) for more explanation (in Chinese).
  */
+ 
+ 
+http://haixiaoyang.wordpress.com/?s=sqrt 
+ 
+    int sqrt(int x) {
+        
+        unsigned long long nBeg = 0;
+        unsigned long long nEnd = x;
+        
+        while (nBeg <= nEnd)
+        {
+            unsigned long long nMid = nBeg + (nEnd-nBeg)/2;
+            if (nMid*nMid == x || (nMid*nMid < x && (nMid+1)*(nMid+1) > x))
+                return nMid;
+            else if (nMid*nMid < x)
+                nBeg = nMid + 1;
+            else nEnd = nMid-1;
+        }
+         
+        return nBeg + (nEnd - nBeg)/2;
+    } 
+ 
  
  
 /* binary search */ 
