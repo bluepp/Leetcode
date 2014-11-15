c71ec14c628a7d53f299aef42b3ac0d52f670a71
bluepp <bluepp@gmail.com>
2014-11-15 22:37:43 +0800

    Update MaxPointsOnALine.h

--- MaxPointsOnALine.h
+++ MaxPointsOnALine.h
@@ -1,5 +1,6 @@
 /* 
     2014-11-12
+    2014-11-15
     bluepp
     May the force be with me!
     
@@ -9,6 +10,52 @@
  Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
  Solution: ...
 */
+
+/*2014-11-15, my version */
+    int maxPoints(vector<Point> &points) {
+        int ret = 0;
+        int n = points.size();
+        unordered_map<double, int> map;
+        
+        for (int i = 0; i < n; i++)
+        {
+            map.clear();
+            int ss = 1, sp = 0;
+            
+            for (int j = i+1; j < n; j++)
+            {
+                double slope = 0.0;
+                
+                if (points[i].x != points[j].x)
+                {
+                    slope = 1.0 * (points[i].y - points[j].y) / (points[i].x - points[j].x);
+                }
+                else if (points[i].y == points[j].y)
+                {
+                    sp ++;
+                    continue;
+                }
+                
+                int tmp = 0;
+                if (map.find(slope) != map.end())
+                {
+                    map[slope]++;
+                    tmp = map[slope];
+                }
+                else
+                {
+                    tmp = map[slope] = 2;
+                }
+                
+                ss = max(ss, tmp);
+            }
+            
+            ret = max(ret, ss+sp);
+        }
+        
+        return ret;
+    }
+
 
     int maxPoints(vector<Point> &points) {
         int N = points.size(), res(0);
