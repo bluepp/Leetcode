0d08122c388d0531062eb48920bd56e78d83c52c
bluepp <bluepp@gmail.com>
2014-11-24 00:29:40 +0800

    Update SubsetsII.h

--- SubsetsII.h
+++ SubsetsII.h
@@ -5,6 +5,7 @@
     2014-08-19
     2014-11-03
     2014-11-20
+    2014-11-24
     
     May the force be with me!
     
@@ -29,6 +30,46 @@
 
  Solution: ..Similar to Subset I, except for line 45.
  */
+ 
+ /* 2014-11-24 My version, easy to understand */
+     vector<vector<int> > subsetsWithDup(vector<int> &s) {
+        vector<int> vec;
+        vector<vector<int> > res;
+        
+        sort(s.begin(), s.end());
+        int n = s.size();
+        
+        for (int i = 0; i <= n; i++)
+        {
+            _subset(s, 0, i, vec, res);
+        }
+        
+        return res;
+    }
+    
+    void _subset(vector<int> &s, int start, int len, vector<int> &vec, vector<vector<int> >&res)
+    {
+        if (vec.size() == len)
+        {
+            res.push_back(vec);
+            return;
+        }
+        
+        int n = s.size();
+        for (int i = start; i < n; i++)
+        {
+            if (i > start && s[i] == s[i-1]) continue;
+            
+            vec.push_back(s[i]);
+            _subset(s, i+1, len, vec, res);
+            vec.pop_back();
+        }
+    }
+ 
+ 
+ 
+ 
+ 
  
  vector<vector<int> > subsetsWithDup(vector<int> &s) {
         vector<vector<int> > res(1, vector<int>());
