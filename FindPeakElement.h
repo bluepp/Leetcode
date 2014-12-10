f07a41b2f151bdb51d7aa03951c037bf20a51252
bluepp <bluepp@gmail.com>
2014-12-10 12:31:03 +0800

    Create FindPeakElement.h

---
+++ FindPeakElement.h
@@ -0,0 +1,32 @@
+/*
+  2014-12-10
+  bluepp
+  May the froce be with me!
+  
+  https://oj.leetcode.com/problems/find-peak-element/
+  
+ A peak element is an element that is greater than its neighbors.
+Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+You may imagine that num[-1] = num[n] = -∞.
+
+For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+*/
+
+    int findPeakElement(const vector<int> &num) {
+        int n = num.size();
+        int left = 0, right = n - 1,mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if ((mid == 0 || num[mid-1] <= num[mid]) && (mid == n -1 || num[mid+1] <= num[mid]))
+                return mid;
+            if (mid > 0 && num[mid - 1] > num[mid]) {
+                right = mid - 1;
+            } else if (num[mid + 1] > num[mid]) {
+                left = mid + 1;
+            }
+        }
+        return mid;
+    }
+
