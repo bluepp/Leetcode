566e65478e631a5538a97a4d44674e3f8e054ca7
bluepp <bluepp@gmail.com>
2014-12-31 16:04:06 +0800

    Update OneEditDistance.h

--- OneEditDistance.h
+++ OneEditDistance.h
@@ -1,12 +1,44 @@
 /*
   bluepp
-  2014-12-09
+  2014-12-31
   May the force be with me!
   
   Given two strings S and T, determine if they are both one edit distance apart.
   
   http://www.danielbit.com/blog/puzzle/leetcode/leetcode-one-edit-distance
 */
+
+/* 2014-12-31, my version */
+    bool isOneEditDistance(string s, string t) {
+        
+        int n1 = s.size(), n2 = t.size();
+        if (abs(n1-n2) > 1) return false;
+        if (n2 > n1) return isOneEditDistance(t, s);
+       
+        int i = 0, j = 0;
+        while (i < n1 && j < n2 && s[i] == t[j])
+        {
+            i++;
+            j++;
+        }
+        
+        if (n1 == n2) 
+        {
+            i++;
+            j++;
+        }
+        else i++;
+        
+        while (i < n1 && j < n2 && s[i]== t[j])
+        {
+            i++;
+            j++;
+        }
+        
+        return (i == n1) && (j == n2);
+       
+    }
+
 
     bool isOneEditDistance(string s, string t) {
         int m = s.size(), n = t.size();
