b16a86c34dc1e76fad000e7a9d794a4a98ac1a9d
bluepp <bluepp@gmail.com>
2015-05-08 15:20:21 +0800

    Update MaximumSubarray.h

--- MaximumSubarray.h
+++ MaximumSubarray.h
@@ -5,6 +5,7 @@
     2014-07-16
     2014-09-02
     2014-09-18
+    2015-05-08
     
     May the force be with me!
     
@@ -16,6 +17,37 @@
 
     Solution: dp.
  */
+ 
+ 
+ /* 2015-05-08 */
+     int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        bool bneg = true;
+        
+        int max_sum = 0, sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (bneg && nums[i] > 0) bneg = false;
+            
+            sum += nums[i];
+            if (sum < 0) sum = 0;
+            
+            max_sum = max(max_sum, sum);
+        }
+       
+        if (bneg)
+        {
+            max_sum = nums[0];
+            for (int i = 1; i < n; i++)
+            {
+                max_sum = max(max_sum, nums[i]);
+            }
+        }
+        
+        return max_sum;
+    }
+ 
+ 
  
  /* 2014-09-18 , my version */
  
