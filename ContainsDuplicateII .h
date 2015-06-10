9fd585553eae68f22ded32bfe8160b4480edd1aa
bluepp <bluepp@gmail.com>
2015-06-10 16:24:26 +0800

    Update ContainsDuplicateII .h

--- ContainsDuplicateII .h
+++ ContainsDuplicateII .h
@@ -1,6 +1,7 @@
 /*
   bluepp
   2015-06-01
+  2015-06-10
   May the force be with me!
   
   Given an array of integers and an integer k, 
@@ -8,6 +9,27 @@
   the array such that nums[i] = nums[j] and the difference between i and j is at most k
   https://leetcode.com/problems/contains-duplicate-ii/
 */
+
+/* 2015-06-10 */
+   bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> map;
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (map.count(nums[i]) && (i-map[nums[i]]) <= k)
+            {
+               return true;
+            }
+            else
+            {
+                map[nums[i]] = i;
+            }
+        }
+        
+        return false;
+    }
+
 
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         int n = nums.size();
