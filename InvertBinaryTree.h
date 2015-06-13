f47ea266b76525daac0e4a23ddfade47b36288c1
bluepp <bluepp@gmail.com>
2015-06-13 16:03:25 +0800

    Create InvertBinaryTree.h

---
+++ InvertBinaryTree.h
@@ -0,0 +1,53 @@
+/*
+  2015-06-13
+  bluepp
+  May the force be with me!
+
+  Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+https://leetcode.com/problems/invert-binary-tree/
+*/
+
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return NULL;
+        
+        queue<TreeNode *> q;
+        q.push(root);
+        q.push(NULL);
+        
+        while (!q.empty())
+        {
+            TreeNode *pCurr = q.front();
+            q.pop();
+            
+            if (pCurr)
+            {
+                TreeNode *pLeft = pCurr->left;
+                pCurr->left = pCurr->right;
+                pCurr->right = pLeft;
+                
+                if (pCurr->left) q.push(pCurr->left);
+                if (pCurr->right) q.push(pCurr->right);
+            }
+            else
+            {
+                if (q.empty()) break;
+                q.push(NULL);
+            }
+        }
+        
+        return root;
+        
+    }
