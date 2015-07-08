60ec91f8d06242684c3717ff67fe3b4c87d916a5
bluepp <bluepp@gmail.com>
2015-07-08 17:16:08 +0800

    Create ReverseLinkedList.h

---
+++ ReverseLinkedList.h
@@ -0,0 +1,49 @@
+/*
+  2015-07-08
+  bluepp
+  May the force be with me!
+  
+  Reverse a singly linked list.
+  https://leetcode.com/problems/reverse-linked-list/
+*/  
+
+/* iteration */
+    ListNode* reverseList(ListNode* head) {
+        
+        if (!head || !head->next) return head;
+        
+        ListNode *pCurr = head, *prev = NULL;
+        while (pCurr)
+        {
+            ListNode *pNext = pCurr->next;
+            
+            pCurr->next = prev;
+            
+            prev = pCurr;
+            pCurr = pNext;
+        }
+        
+        return prev;
+    }
+    
+    
+    /* recursion */
+       ListNode* reverseList(ListNode* head) {
+        
+        reverse(head);
+        
+        return head;
+    }
+    
+    void reverse(ListNode *&head)
+    {
+        if (!head) return;
+        
+        ListNode *rest = head->next;
+        if (!rest) return;
+        
+        reverse(rest);
+        head->next->next = head;
+        head->next = NULL;
+        head = rest;
+    }
