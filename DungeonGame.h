67d680433c0e722c6c30025f1d4df1b671830cc2
bluepp <bluepp@gmail.com>
2015-07-10 17:02:15 +0800

    Create DungeonGame.h

---
+++ DungeonGame.h
@@ -0,0 +1,52 @@
+/*
+  2015-07-10
+  bluepp
+  May the force be with me!
+  
+The demons had captured the princess (P) and imprisoned her in the bottom-right corner of a dungeon.
+The dungeon consists of M x N rooms laid out in a 2D grid. Our valiant knight (K) was initially positioned 
+in the top-left room and must fight his way through the dungeon to rescue the princess.
+
+The knight has an initial health point represented by a positive integer. 
+If at any point his health point drops to 0 or below, he dies immediately.
+
+Some of the rooms are guarded by demons, so the knight loses health (negative integers) 
+upon entering these rooms; other rooms are either empty (0's) or contain magic orbs that 
+increase the knight's health (positive integers).
+
+In order to reach the princess as quickly as possible, the knight decides to 
+move only rightward or downward in each step.
+
+
+Write a function to determine the knight's minimum initial health so that he is able to rescue the princess
+
+https://leetcode.com/problems/dungeon-game/
+*/
+
+/* http://www.fgdsb.com/2015/01/06/harry-potter-game/ */
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        if (dungeon.empty() || dungeon[0].empty()) return 0;
+        
+        int m = dungeon.size(), n = dungeon[0].size();
+        vector<vector<int> >dp(m, vector<int>(n, 0));
+        
+        for (int i = m-1; i >= 0; i--)
+        {
+            for (int j = n-1; j >= 0; j--)
+            {
+                if (i == m-1 && j == n-1)
+                {
+                    dp[i][j] = max(1, 1-dungeon[i][j]);
+                }
+                else
+                {
+                    int right = j == n-1 ? INT_MAX : dp[i][j+1]-dungeon[i][j];
+                    int down = i == m-1 ? INT_MAX : dp[i+1][j]- dungeon[i][j];
+                    dp[i][j] = max(1, min(right, down));
+                }
+            }
+        }
+        
+        return dp[0][0];
+        
+    }
