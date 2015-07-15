ae2fbd08fe9b1d9d57eb48d64f9f504951c06538
bluepp <bluepp@gmail.com>
2015-07-15 11:17:06 +0800

    Update NumberofDigitOne.h

--- NumberofDigitOne.h
+++ NumberofDigitOne.h
@@ -40,3 +40,26 @@ https://leetcode.com/problems/number-of-digit-one/
         
         return res;
     }
+    
+    
+/* Follow Up: you can change the code to meet the new problem Number of Digit K, such as Number of Digit 3 */
+
+
+int countDigitK(int n, int k) {
+    long long base = 1, res = 0, last = 0;
+    while(n >= base){
+        int index = (n / base) % 10;
+        long long remain = n - (n / base) * base;
+        if(index > k){
+            res = res + index * last + base;
+        }else if(index == k){
+            res = res + index * last + remain + 1;
+        }else{
+            res = res + index * last;
+        }
+        last = last * 10 + base;
+        base = base * 10;
+    }
+    return res;
+}
+
