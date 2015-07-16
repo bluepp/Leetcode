00e42af387427d01212993a705d2c67132008c26
bluepp <bluepp@gmail.com>
2015-07-16 11:02:28 +0800

    Update UpsidedownBinaryTree.h

--- UpsidedownBinaryTree.h
+++ UpsidedownBinaryTree.h
@@ -44,22 +44,19 @@ solution: 1. iteration http://www.danielbit.com/blog/puzzle/leetcode/leetcode-bi
        
     }
     
-/* recursion */
-   TreeNode *upsideDownBinaryTree(TreeNode *root) {
-        TreeNode *temp, *newRoot = NULL;
-        temp = buildUpsideDownBT(root, newRoot);
-        return newRoot;
-    }
     
-    TreeNode *buildUpsideDownBT(TreeNode *root, TreeNode *&newRoot) {
-        if(!root) return root;
-        if(!root->left && !root->right) {
-            newRoot = root;
-            return root;
-        }
-        TreeNode *parent = buildUpsideDownBT(root->left, newRoot);
-        parent->left = root->right;
-        parent->right = root;
-        root->left = root->right = NULL;
-        return parent->right;
+    /* 2015-07-16, update */
+    /* http://www.fgdsb.com/2015/01/04/binary-tree-upside-down/ */
+    TreeNode* upsideDownBinaryTree(TreeNode* root) {
+        if (!root || !root->left) return root;
+        
+        TreeNode *ret = upsideDownBinaryTree(root->left);
+        
+        root->left->left = root->right;
+        root->left->right = root;
+        
+        root->left = NULL;
+        root->right = NULL;
+        
+        return ret;
     }
