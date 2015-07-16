3be360ee570472b2f396e2fce188c166e486ce2f
bluepp <bluepp@gmail.com>
2015-07-16 16:03:43 +0800

    Create ReadNCharactersGivenRead4II.h

---
+++ ReadNCharactersGivenRead4II.h
@@ -0,0 +1,17 @@
+/*
+  2015-07-16
+  bluepp
+  May the force be with me!
+  
+The API: int read4(char *buf) reads 4 characters at a time from a file.
+
+The return value is the actual number of characters read. 
+For example, it returns 3 if there is only 3 characters left in the file.
+
+By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
+
+Note:
+The read function may be called multiple times.
+*/
+
+/* http://www.cnblogs.com/EdwardLiu/p/4240616.html */
