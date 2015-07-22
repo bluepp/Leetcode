0f6f1b3ebfe9c87d8b88700101fc338158ff2cfa
bluepp <bluepp@gmail.com>
2015-07-22 11:05:42 +0800

    Update CountPrimes.h

--- CountPrimes.h
+++ CountPrimes.h
@@ -11,32 +11,32 @@ https://leetcode.com/problems/count-primes/
     int countPrimes(int n) {
         if (n <= 2) return 0;
         if (n == 3) return 1;
-        bool *prime = (bool *)malloc(sizeof(bool) * n);
-        int i = 0, j = 0;
-        int count = n-2;
-        int r = sqrt(n);
         
-        for (j = 0; j < n; j++)
+        int count = n-2;
+        bool prime[n];
+        
+        for (int i = 0; i < n; i++)
         {
-            prime[j] = 1;
+            prime[i] = true;
         }
         
-        for (i = 2; i <= r; i++)
+        int r = sqrt(n);
+        for (int i = 2; i <= r; i++)
         {
             if (prime[i])
             {
-                for (j = i*2; j < n; j += i)
+                for (int j = i*2; j < n; j += i)
                 {
                     if (prime[j])
                     {
-                        prime[j] = 0;
-                        count--;
+                        prime[j] = false;
+                        count --;
                     }
                 }
             }
         }
         
-        free(prime);
         return count;
+        
     }
   
