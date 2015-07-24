60821349e70fa2139cd256cb98d55571cca266a1
bluepp <bluepp@gmail.com>
2015-07-24 11:15:58 +0800

    Update BinaryTreePostorderTraversal.h

--- BinaryTreePostorderTraversal.h
+++ BinaryTreePostorderTraversal.h
@@ -63,8 +63,76 @@
         return res;
     }
     
+    /* ---------------------------------- */
+    /* Morris , 2015-07-24 */
+        vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        TreeNode dump(0);
+        dump.left = root;
+        TreeNode *cur = &dump, *prev = NULL;
+        while (cur)
+        {
+            if (cur->left)
+            {
+                prev = cur->left;
+                while (prev->right != NULL && prev->right != cur)
+                    prev = prev->right;
+
+                if (prev->right == NULL)
+                {
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+                else
+                {
+                    printReverse(cur->left, prev, res);  // call print
+                    prev->right = NULL;
+                    cur = cur->right;
+                }
+            }
+            else
+            {
+                cur = cur->right;
+            }
+        }
+        
+        return res;
+    }
+        
+    void printReverse(TreeNode* from, TreeNode *to, vector<int>& res) // print the reversed tree nodes 'from' -> 'to'.
+    {
+        reverse(from, to);
+        
+        TreeNode *p = to;
+        while (true)
+        {
+            res.push_back(p->val);
+            if (p == from)
+                break;
+            p = p->right;
+        }
+        
+        reverse(to, from);
+    }
     
-    -----------------------------
+        
+    void reverse(TreeNode *from, TreeNode *to) // reverse the tree nodes 'from' -> 'to'.
+    {
+        if (from == to)
+            return;
+        TreeNode *x = from, *y = from->right, *z;
+        while (true)
+        {
+            z = y->right;
+            y->right = x;
+            x = y;
+            y = z;
+            if (x == to)
+                break;
+        }
+    }
+    
+    /* ----------------------------- */
     
     vector<int> postorderTraversal(TreeNode *root) {
         vector<int> res;
