1fead58b1fb5c49e17433d02c90fce7cf9ce941d
bluepp <bluepp@gmail.com>
2015-08-11 14:35:38 +0800

    Create MeetingRooms.h

---
+++ MeetingRooms.h
@@ -0,0 +1,37 @@
+/*
+  2015-08-11
+  bluepp
+  May the force be with me!
+  
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), 
+determine if a person could attend all meetings.
+
+For example,
+Given [[0, 30],[5, 10],[15, 20]],
+return false.
+
+https://leetcode.com/problems/meeting-rooms/
+*/
+
+bool mycompare (Interval a, Interval b)
+{
+    return a.start < b.start;
+}
+ 
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        
+        int n = intervals.size();
+        if (n == 0 || n == 1) return true;
+        
+        sort(intervals.begin(), intervals.end(), mycompare);
+        
+        for (int i = 1; i < n; i++)
+        {
+            if (intervals[i].start < intervals[i-1].end) return false;
+        }
+        
+        return true;
+    }
+};
