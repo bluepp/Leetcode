391222a9b2400f401910b67f2a49033ee43e8bc7
bluepp <bluepp@gmail.com>
2015-08-17 16:35:03 +0800

    Update CountUnivalueSubtrees.h

--- CountUnivalueSubtrees.h
+++ CountUnivalueSubtrees.h
@@ -18,6 +18,57 @@ return 4.
 https://leetcode.com/problems/count-univalue-subtrees/
 */
 
+
+/* 2015-08-17, my version */
+
+    int countUnivalSubtrees(TreeNode* root) {
+        
+        int count = 0;
+        _count(root, count);
+        return count;
+    }
+    
+    bool _count(TreeNode *root, int &count)
+    {
+        if (!root)
+        {
+            count = 0;
+            return true;
+        }
+        
+        if (!root->left && !root->right)
+        {
+            count = 1;
+            return true;
+        }
+        
+        int lcount = 0, rcount = 0;
+        bool l = _count(root->left, lcount);
+        bool r = _count(root->right, rcount);
+        
+        if (l && r)
+        {
+            if (root->left && root->left->val != root->val || root->right && root->right->val != root->val)
+            {
+                count = lcount+rcount;
+                return false;
+            }
+            else
+            {
+                count = lcount+rcount+1;
+                return true;
+            }
+        }
+        else
+        {
+            count = lcount+rcount;
+            return false;
+        }
+    }
+
+
+
+
     int countUnivalSubtrees(TreeNode* root) {
         if (!root) return 0;
         
