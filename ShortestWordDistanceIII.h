63f880c6cdf27ccc6da91a9133f0342ce014d609
bluepp <bluepp@gmail.com>
2015-08-18 16:27:27 +0800

    Update ShortestWordDistanceIII.h

--- ShortestWordDistanceIII.h
+++ ShortestWordDistanceIII.h
@@ -21,22 +21,21 @@ https://leetcode.com/problems/shortest-word-distance-iii/
 */
 
 /* no hash map, ? https://leetcode.com/discuss/50715/12-16-lines-java-c */
+/* update, 2015-08-18 */
     int shortestWordDistance(vector<string>& words, string word1, string word2) {
         
-        long long dist = INT_MAX, i1 = dist, i2 = -dist;
+        long long dist = INT_MAX, i1 = -1, i2 = -1;
         
-        for (int i = 0; i < words.size(); i++)
+        for (int i=0; i<words.size(); i++)
         {
             if (words[i] == word1)
                 i1 = i;
-            if (words[i] == word2)
-            {
+            if (words[i] == word2) {
                 if (word1 == word2)
                     i1 = i2;
                 i2 = i;
             }
-            
-            dist = min(dist, abs(i1 - i2));
+            if (i1 != -1 && i2 != -1) dist = min(dist, abs(i1 - i2));
         }
         
         return dist;
