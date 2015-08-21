b83cf06b54c91d25aac6ce610ab857445e936484
bluepp <bluepp@gmail.com>
2015-08-21 11:20:05 +0800

    Create PalindromePermutation.h

---
+++ PalindromePermutation.h
@@ -0,0 +1,48 @@
+/*
+  2015-08-21
+  bluepp
+  May the force be with me!
+  
+  Given a string, determine if a permutation of the string could form a palindrome.
+
+  For example,
+  "code" -> False, "aab" -> True, "carerac" -> True.
+  https://leetcode.com/problems/palindrome-permutation/
+*/
+
+    bool canPermutePalindrome(string s) {
+        int n = s.length();
+        if (n < 2) return true;
+        
+        int x = s[0]-'a';
+        int t = 0;
+        for (int i = 1; i < n; i++)
+        {
+            x ^= s[i] - 'a';
+        }
+        
+        if (n%2 != 0)
+        {
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (x == s[i]-'a') 
+                {
+                    index = i;
+                    break;
+                }
+            }
+            
+            t= s[0]-'a';
+            for (int i = 1; i < n; i++)
+            {
+                if (i == index) continue;
+                t ^= s[i]-'a';
+            }
+            
+            x = t;
+        }
+        
+        return x == 0;
+    }
+  
