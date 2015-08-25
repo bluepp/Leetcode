7c1b6655fea9311a8d0aab544dcf72986751f696
bluepp <bluepp@gmail.com>
2015-08-25 17:09:02 +0800

    Create ShortestPalindrome.h

---
+++ ShortestPalindrome.h
@@ -0,0 +1,71 @@
+/*
+  2015-08-25
+  bluepp
+  May the force be with me!
+  
+Given a string S, you are allowed to convert it to a palindrome by adding characters in front of it.
+Find and return the shortest palindrome you can find by performing this transformation.
+
+For example:
+
+Given "aacecaaa", return "aaacecaaa".
+Given "abcd", return "dcbabcd".
+https://leetcode.com/problems/shortest-palindrome/
+*/
+
+/* https://leetcode.com/discuss/36807/c-8-ms-kmp-based-o-n-time-&-o-n-memory-solution */
+/* O(N) */
+
+    string shortestPalindrome(string s) {
+    
+        string rev_s = s;
+        reverse(rev_s.begin(), rev_s.end());
+        string l = s + "#" + rev_s;
+
+        vector<int> p(l.size(), 0);
+        for (int i = 1; i < l.size(); i++) {
+            int j = p[i - 1];
+            while (j > 0 && l[i] != l[j])
+                j = p[j - 1];
+            p[i] = (j += l[i] == l[j]);
+        }
+
+        return rev_s.substr(0, s.size() - p[l.size() - 1]) + s;
+    }
+
+
+
+/* https://leetcode.com/discuss/40486/accepted-solution-different-based-solution-easy-understand */
+/* O(N2) */
+
+    string shortestPalindrome(string s) {
+        int n = s.length();
+        if (n < 2) return s;
+        
+        int longest = 1, start, end;
+        for (int begin = 0; begin <= n/2; )
+        {
+            start = end = begin;
+            
+            while (end < n-1 && s[end+1] == s[end]) ++end;
+            begin = end+1;
+            
+            while (end < n-1 && start > 0 && s[end+1] == s[start-1])
+            {
+                ++end;
+                --start;
+            }
+            
+            if (start == 0 && longest < end-start+1)
+            {
+                longest = end-start+1;
+            }
+        }
+        
+        string remain = s.substr(longest, n-longest);
+        reverse(remain.begin(), remain.end());
+        
+        return remain+s;
+    }
+
+  
