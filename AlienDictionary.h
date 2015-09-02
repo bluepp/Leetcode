15b829bfe8261a6ccc96443be3256537f7499ccc
bluepp <bluepp@gmail.com>
2015-09-02 15:08:19 +0800

    Update AlienDictionary.h

--- AlienDictionary.h
+++ AlienDictionary.h
@@ -27,6 +27,55 @@ https://leetcode.com/problems/alien-dictionary/
 
 */
 
+/* https://leetcode.com/discuss/54188/16-18-lines-python-29-lines-c */
+
+   string alienOrder(vector<string>& words) {
+        map<char, set<char>> suc;
+        map<char, int> pre;
+        set<char> chars;
+        string s;
+        
+        for (string t : words)
+        {
+            chars.insert(t.begin(), t.end());
+            for (int i = 0; i < min(s.size(), t.size()); i++)
+            {
+                char a = s[i], b = t[i];
+                if (a != b)
+                {
+                    suc[a].insert(b);
+                    pre[b]++;
+                    break;
+                }
+            }
+            s = t;
+        }
+        
+        set<char> free = chars;
+        for (auto ci : pre)
+        {
+            free.erase(ci.first);
+        }
+        string order;
+        while (free.size())
+        {
+            char a = *begin(free);
+            free.erase(a);
+            order += a;
+            
+            for (char b : suc[a])
+            {
+                if (--pre[b] == 0)
+                {
+                    free.insert(b);
+                }
+            }
+        }
+        
+        return order.size() == chars.size() ? order : "";
+    }
+
+
 
 /* topo sort, does not pass */
 class Graph
