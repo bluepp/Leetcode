a2d20a03f24ee320b9470582d00e068424e01f6a
bluepp <bluepp@gmail.com>
2015-09-07 14:19:24 +0800

    Update LongestSubstringwithAtMostTwoDistinctCharacters.h

--- LongestSubstringwithAtMostTwoDistinctCharacters.h
+++ LongestSubstringwithAtMostTwoDistinctCharacters.h
@@ -68,6 +68,50 @@ https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-charac
         return ret;
     }
 
+/* 2015-09-07, hash */
+   int lengthOfLongestSubstringTwoDistinct(string s) {
+        int n = s.length();
+        unordered_map<char, int> map;
+        
+        int ret = 0;
+        int start = 0;
+        char first = '0', second = '0';
+        for (int i = 0; i < n; i++)
+        {
+            if (first == '0') 
+            {
+                first = s[i];
+            }
+            else if (s[i] != first && second == '0') 
+            {
+                second = s[i];
+            }
+            else if (s[i] != first && s[i] != second)
+            {
+                if (map[first] < map[second])
+                {
+                    start = map[first]+1;
+                    first = second;
+                }
+                else
+                {
+                    start = map[second]+1;
+                }
+                
+                second = s[i];
+            }
+            
+            map[s[i]] = i;
+            ret = max(ret, i-start+1);
+            
+        }
+        
+        ret = max(ret, n-start);
+        return ret;
+    }
+    
+
+
 
 
 /* 2015-07-22, update, my version */
