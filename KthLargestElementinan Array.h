3dc0e3b329386186d8ddd5a510bfb3458f2ea031
bluepp <bluepp@gmail.com>
2015-09-11 14:38:24 +0800

    Update KthLargestElementinan Array.h

--- KthLargestElementinan Array.h
+++ KthLargestElementinan Array.h
@@ -18,6 +18,37 @@ https://leetcode.com/problems/kth-largest-element-in-an-array/
 /* quick sort, O(N) average */
 /* https://leetcode.com/discuss/38336/solutions-partition-priority_queue-multiset-respectively */
 
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        int l = 0, r = n-1;
+        while (true)
+        {
+            int pos = partition(nums,l, r);
+            
+            if (pos == k-1) return nums[pos];
+            if (pos > k-1) r = pos-1;
+            else l = pos+1;
+        }
+    }
+    
+    int partition(vector<int> &nums, int left, int right)
+    {
+        int pivot = nums[left];
+        int l = left+1, r = right;
+        while (l <= r)
+        {
+            if (nums[l] < pivot && pivot < nums[r]) swap(nums[l++], nums[r--]);
+            if (nums[l] >= pivot) l++;
+            if (nums[r] <= pivot) r--;
+        }
+        swap(nums[left], nums[r]);
+        return r;
+    }
+
+
+
+/* https://leetcode.com/discuss/38336/solutions-partition-priority_queue-multiset-respectively */
+
 
     int findKthLargest(vector<int>& nums, int k) {
         int n = nums.size();
