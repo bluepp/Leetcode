d587b7c4a02c02e3ec05390990994bf09f19fd82
bluepp <bluepp@gmail.com>
2016-06-02 02:30:59 +0800

    Create FlipGame.h

---
+++ FlipGame.h
@@ -0,0 +1,36 @@
+/*
+bluepp
+2016-06-02
+May the force be with me!
+
+https://leetcode.com/problems/flip-game/
+You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
+
+Write a function to compute all possible states of the string after one valid move.
+
+For example, given s = "++++", after one move, it may become one of the following states:
+
+[
+  "--++",
+  "+--+",
+  "++--"
+]
+If there is no valid move, return an empty list [].
+*/
+
+
+   vector<string> generatePossibleNextMoves(string s) {
+        vector<string> result;
+    
+        for (int i = 0; i<(int)(s.length() - 1); i++) {
+            
+            if (s[i] == '+' && s[i + 1] == '+') {
+                s[i] = '-', s[i + 1] = '-';
+                result.push_back(s);
+                s[i] = '+', s[i + 1] = '+';
+            }
+        }
+
+        return result;
+       
+    }
