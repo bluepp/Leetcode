51fa4a645b32151e2664ec8dc1a965cc66d02b0e
bluepp <bluepp@gmail.com>
2016-06-01 16:21:53 +0800

    Create NestedListWeightSum.h

---
+++ NestedListWeightSum.h
@@ -0,0 +1,39 @@
+/*
+bluepp
+2016-06-01
+May the force be with me!
+
+https://leetcode.com/problems/nested-list-weight-sum/
+Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Example 1:
+Given the list [[1,1],2,[1,1]], return 10. (four 1's at depth 2, one 2 at depth 1)
+
+Example 2:
+Given the list [1,[4,[6]]], return 27. (one 1 at depth 1, one 4 at depth 2, and one 6 at depth 3; 1 + 4*2 + 6*3 = 27)
+*/
+
+class Solution {
+public:
+
+    int sum = 0, depth = 1;
+    int depthSum(vector<NestedInteger>& nestedList) {
+        
+        for (auto p : nestedList)
+        {
+            if (p.isInteger())
+            {
+                sum += p.getInteger()*depth;
+            }
+            else
+            {
+                depth ++;
+                depthSum(p.getList());
+                depth--;
+            }
+        }
+        
+        return sum;
+    }
