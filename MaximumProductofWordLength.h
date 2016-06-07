0b0fd1a38484568f78aa0ad7e9e41a5a90013386
bluepp <bluepp@gmail.com>
2016-06-08 03:52:43 +0800

    Create MaximumProductofWordLength.h

---
+++ MaximumProductofWordLength.h
@@ -0,0 +1,54 @@
+/*
+bluepp
+2016-06-08
+May the force be with me!
+
+https://leetcode.com/problems/maximum-product-of-word-lengths/
+
+Given a string array words, find the maximum value of length(word[i]) * length(word[j]) where the two words do not share common letters. You may assume that each word will contain only lower case letters. If no such two words exist, return 0.
+
+Example 1:
+Given ["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]
+Return 16
+The two words can be "abcw", "xtfn".
+
+Example 2:
+Given ["a", "ab", "abc", "d", "cd", "bcd", "abcd"]
+Return 4
+The two words can be "ab", "cd".
+
+Example 3:
+Given ["a", "aa", "aaa", "aaaa"]
+Return 0
+No such pair of words.
+
+*/
+
+   int maxProduct(vector<string>& words) {
+        
+        int ret = 0;
+        int n = words.size();
+        vector<int> vec(n, 0);
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < words[i].length(); j++)
+            {
+                vec[i] |= 1 << (words[i][j] - 'a');
+            }
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                if (vec[i] & vec[j]) continue;
+                
+                int t1 = words[i].length(), t2 = words[j].length();
+                ret = max(ret, t1*t2);
+            }
+        }
+        
+        return ret;
+        
+    }
