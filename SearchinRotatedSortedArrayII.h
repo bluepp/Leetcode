a1e72db09cde01727891e064ca38e33448552cb0
bluepp <bluepp@gmail.com>
2016-06-11 04:11:09 +0800

    Update SearchinRotatedSortedArrayII.h

--- SearchinRotatedSortedArrayII.h
+++ SearchinRotatedSortedArrayII.h
@@ -19,6 +19,7 @@
            Since there are duplicates, it's hard to decide which branch to go if binary-search is deployed.
  */
  
+ 
     bool search(int a[], int n, int target) {
         
         for (int i = 0; i < n; i++)
@@ -28,4 +29,28 @@
         }
         
         return false;
+    }
+
+
+/* 2016-06-11, add binary search */
+/* https://leetcode.com/discuss/94293/simple-c-solution-explained */
+
+   bool search(vector<int>& nums, int target) {
+        
+        int l = 0, r = nums.size() - 1;        
+        while(l <= r){  
+            int mid = l + (r - l) / 2;            
+            if(nums[mid] == target) return true;
+            if(nums[mid] > nums[r]){
+                if(target > nums[mid] || target <= nums[r]) l = mid + 1;    
+                else r = mid - 1;                                                               
+            }else if(nums[mid] == nums[r]){
+                r --;   // may cause linear time here, e.g. [7, 8, 9, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], search for 0
+            }
+            else{
+                if(target <= nums[r] && target > nums[mid]) l = mid + 1; 
+                else r = mid - 1;                                                               
+            }
+        }
+        return false;   
     }
