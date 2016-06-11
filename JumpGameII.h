8ac39d695a023102d2a92965f69135747e820c8f
bluepp <bluepp@gmail.com>
2016-06-12 03:29:39 +0800

    Update JumpGameII.h

--- JumpGameII.h
+++ JumpGameII.h
@@ -25,6 +25,58 @@
     Solution: Jump to the position where we can jump farthest (index + A[index]) next time.
  */
  
+ 
+/* 2016-06-12, pass */
+/* https://leetcode.com/discuss/95897/my-very-simple-c-solution */
+ 
+ 
+     int jump(vector<int>& nums) {
+       
+        int steps = 0, s1 = 0, s2 = 0;// take steps can reach s1, take steps+1 can reach s2
+        for (int i = 0; i < nums.size(); ++i) {
+        
+            if (s1 >= nums.size()-1) return steps;
+        
+            s2 = max(nums[i]+i,s2);
+            if (i >= s1) {//steps is not enough, update s1 to s2 and increase steps
+        
+                s1 = s2;
+                steps++;
+            }
+        }
+        
+        return steps;
+    }
+ 
+ 
+ 
+ /* 2016-06-12 TLE */
+    int jump(vector<int>& nums) {
+       
+       int n = nums.size();
+       int count = 0;
+       int start = 0;
+       
+       while (start < n-1)
+       {
+           count++;
+           int far = nums[start]+start;
+           if (far >= n-1) return count;
+           
+           int max = start;
+           for (int j = start+1; j <= far; j++)
+           {
+               if (j+nums[j] > start+nums[start]) max = start;
+           }
+           
+           start = max;
+       }
+       
+       return count;
+       
+    }
+ 
+ 
     int jump(int A[], int n) {
         int count = 0;
         int start = 0;
