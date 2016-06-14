21ac9a601e8a89d7590e4798fd8c0004278212f6
bluepp <bluepp@gmail.com>
2016-06-14 17:15:42 +0800

    Update WildcardMatching.h

--- WildcardMatching.h
+++ WildcardMatching.h
@@ -31,21 +31,21 @@ isMatch("aab", "c*a*b") → false
     bool isMatch(string s, string p) {
         
         int m = s.length(), n = p.length();
-        int i = 0, j = 0, asterick = -1, match;
+        int i = 0, j = 0, asterisk = -1, match;
         
         while (i < m) {
         
             if (j < n && p[j] == '*') {
                 match = i;  
-                asterick = j++;
+                asterisk = j++;
             }
             else if (j < n && (s[i] == p[j] || p[j] == '?')) {
                 i++;
                 j++;
             }
-            else if (asterick >= 0) {
+            else if (asterisk >= 0) {
                 i = ++match;
-                j = asterick + 1;
+                j = asterisk + 1;
             }
             else return false;
         }
