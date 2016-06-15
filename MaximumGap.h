636f434fa1090239bf5cf20bae8fa2f89c97533b
bluepp <bluepp@gmail.com>
2016-06-15 08:16:46 +0800

    Update MaximumGap.h

--- MaximumGap.h
+++ MaximumGap.h
@@ -15,7 +15,38 @@ https://leetcode.com/discuss/53636/radix-sort-solution-in-java-with-explanation
 
 
 /* bucket sort */
-/* https://leetcode.com/discuss/34289/pigeon-hole-principle */
+/* https://leetcode.com/discuss/36502/my-concise-and-short-c-code-with-comment-explanation */
+int maximumGap(vector<int>& nums) {
+    int n = nums.size();
+    if( n < 2 ) return 0;
+    int maxE = *max_element(nums.begin(),nums.end());
+    int minE = *min_element(nums.begin(),nums.end());
+
+    int len = maxE - minE;
+    if( len <= 1 ) return len;
+    vector<int> buck_max(n, INT_MIN);
+    vector<int> buck_min(n, INT_MAX);
+
+    for(int i = 0; i < n; i++) {
+        // note the divide and multiply order and the double cast
+        // it's used to avoid the overflow and underflow during calculation
+        int index = (double)( nums[i] - minE )  / len * ( n - 1 );
+        buck_max[index] = max(buck_max[index], nums[i]);
+        buck_min[index] = min(buck_min[index], nums[i]);
+    }
+
+    int gap = 0, pre = buck_max[0];
+    for(int i = 1; i < n; i++) {
+        if( buck_max[i] == INT_MIN ) continue;
+        gap = max(gap, buck_min[i] - pre);
+        pre = buck_max[i];
+    }
+    return gap;
+}
+
+
+
+
 
     int maximumGap(vector<int>& nums) {
         int n = nums.size();
