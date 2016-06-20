34eac76121a14492fae8b58b87655cfaa45e0d7f
bluepp <bluepp@gmail.com>
2016-06-20 10:25:17 +0800

    Update PaintFence.h

--- PaintFence.h
+++ PaintFence.h
@@ -11,6 +11,23 @@ Note:
 n and k are non-negative integers.
 https://leetcode.com/problems/paint-fence/
 */
+/*
+https://leetcode.com/discuss/56173/o-n-time-java-solution-o-1-space
+*/
+
+public int numWays(int n, int k) {
+    if(n == 0) return 0;
+    else if(n == 1) return k;
+    int diffColorCounts = k*(k-1);
+    int sameColorCounts = k;
+    for(int i=2; i<n; i++) {
+        int temp = diffColorCounts;
+        diffColorCounts = (diffColorCounts + sameColorCounts) * (k-1);
+        sameColorCounts = temp;
+    }
+    return diffColorCounts + sameColorCounts;
+}
+
 
 /*dp,  https://leetcode.com/discuss/56146/dynamic-programming-c-o-n-time-o-1-space-0ms */
     int numWays(int n, int k) {
