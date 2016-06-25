ce8d9f7ffd63194d22d127e4247b117f62481c9b
bluepp <bluepp@gmail.com>
2016-06-26 03:22:32 +0800

    Update RegularExpressionMatching.h

--- RegularExpressionMatching.h
+++ RegularExpressionMatching.h
@@ -25,6 +25,40 @@
  Solution: Both of the two solutions are from http://leetcode.com/2011/09/regular-expression-matching.html .
 */
 
+/* 2016-06-26 , update */
+
+   bool isMatch(string s, string p) {
+        
+        if (p.empty())
+        {
+            return s.empty();
+        }
+        
+        if (p.size() == 1)
+        {
+            return (s.size() == 1 && (s[0] == p[0] || p[0] == '.'));
+        }
+        
+        if (p[1] != '*')
+        {
+            if (s.empty()) return false;
+            return (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1));
+        }
+        
+        while (!s.empty() && (s[0] == p[0] || p[0] == '.'))
+        {
+            if (isMatch(s, p.substr(2)))
+            {
+                return true;
+            }
+            
+            s = s.substr(1);
+        }
+        
+        return isMatch(s, p.substr(2));
+        
+    }
+
     bool isMatch(const char *s, const char *p) {
         if (*p == '\0')
             return *s == '\0';
