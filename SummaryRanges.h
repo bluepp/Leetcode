27f781d8bd2d431317297c34cca808260a9a3805
bluepp <bluepp@gmail.com>
2016-06-26 01:19:56 +0800

    Update SummaryRanges.h

--- SummaryRanges.h
+++ SummaryRanges.h
@@ -8,6 +8,33 @@ For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
 
 */
 
+/* 2016-06-26 , update */
+    vector<string> summaryRanges(vector<int>& nums) {
+        
+        vector<string> res;
+        int i = 0, n = nums.size();
+        
+        while (i < n)
+        {
+            int j = 1;
+            
+            while (i+j < n && nums[i+j]-nums[i] == j)
+            {
+                j++;
+            }
+            
+            res.push_back(j <= 1 ? to_string(nums[i]) : to_string(nums[i]) + "->" + to_string(nums[i+j-1]));
+            
+            i += j;
+        }
+        
+        return res;
+        
+    }
+
+
+
+
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> res;
         
