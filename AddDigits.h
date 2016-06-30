ab2bb98b8f62c67572f7398af56239009d6401c6
bluepp <bluepp@gmail.com>
2016-06-30 23:29:36 +0800

    Update AddDigits.h

--- AddDigits.h
+++ AddDigits.h
@@ -15,6 +15,15 @@ Could you do it without any loop/recursion in O(1) runtime?
 https://leetcode.com/problems/add-digits/
 */
 
+
+/* 2016-06-30, another one */
+   int addDigits(int num) {
+        
+        return (num - 1) % 9 + 1;
+      
+    }
+
+
 /* no recursion, no loop, https://leetcode.com/discuss/52160/one-line-solution */
     int addDigits(int num) {
         return num%9 == 0? (num==0?0:9) : num % 9;
