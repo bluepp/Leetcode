958b98f6e55bc73179de4c5d7613e95550926006
bluepp <bluepp@gmail.com>
2016-07-01 21:13:29 +0800

    Update IsomorphicString.h

--- IsomorphicString.h
+++ IsomorphicString.h
@@ -17,6 +17,26 @@ Given "foo", "bar", return false.
 Given "paper", "title", return true.
 https://leetcode.com/problems/isomorphic-strings/
 */
+
+/* 2016-07-01, a simple one */
+    bool isIsomorphic(string s, string t) {
+        
+        int n = s.length();
+        vector<int> m1(256, 0), m2(256, 0);
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (m1[s[i]] != m2[t[i]])
+            {
+                return false;
+            }
+            
+            m1[s[i]] = i+1;
+            m2[t[i]] = i+1;
+        }
+        
+        return true;
+    }
   
      bool isIsomorphic(string s, string t) {
         int m = s.size(), n = t.size();
