bca5bce3362e6ddc5e7e126fe0e455e9e1c52219
bluepp <bluepp@gmail.com>
2016-07-13 02:18:12 +0800

    Update GasStation.h

--- GasStation.h
+++ GasStation.h
@@ -17,6 +17,36 @@
  Solution: ...
 */
 
+/* 2016-07-13 , update */
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        
+        int total = 0, start = 0, sum = 0;
+        int n = gas.size();
+        
+        for (int i = 0; i < n; i++)
+        {
+            total += gas[i] - cost[i];
+            sum += gas[i] - cost[i];
+            
+            if (sum < 0)
+            {
+                start = i+1;
+                sum = 0;
+            }
+        }
+        
+        if (total < 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return start;
+        }
+        
+    }
+
+
 /* my solution, time limit exceed , bad!! */
     int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
         int n = gas.size();
