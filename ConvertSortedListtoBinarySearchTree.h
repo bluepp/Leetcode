7793da12e90d44c380318ff25f0a37d017297614
bluepp <bluepp@gmail.com>
2016-07-12 22:27:35 +0800

    Update ConvertSortedListtoBinarySearchTree.h

--- ConvertSortedListtoBinarySearchTree.h
+++ ConvertSortedListtoBinarySearchTree.h
@@ -13,6 +13,34 @@
     Solution: Recursion. Pre-order. O(n)
 */
 
+/* 2016-07-12, update */
+
+    TreeNode* sortedListToBST(ListNode* head) {
+        
+        if (!head) return NULL;
+        if (!head->next) return new TreeNode(head->val);
+        
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *last = slow;
+        
+        while (fast->next && fast->next->next) {
+            last = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        fast = slow->next;
+        last->next = NULL;
+        
+        TreeNode *cur = new TreeNode(slow->val);
+        if (head != slow) cur->left = sortedListToBST(head);
+        cur->right = sortedListToBST(fast);
+        
+        return cur;
+    }
+
+
 /* my version */
 /* 2014-07-09 */
 
