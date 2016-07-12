9b912494560f4ca4b384d94d8d29240edf14fc74
bluepp <bluepp@gmail.com>
2016-07-13 00:59:38 +0800

    Update CourseSchedule.h

--- CourseSchedule.h
+++ CourseSchedule.h
@@ -24,6 +24,40 @@ So it is impossible.
 https://leetcode.com/problems/course-schedule/
 */
 
+/* bfs, 2016-07-13, update */
+
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        
+        vector<vector<int> > graph(numCourses, vector<int>(0));
+        vector<int> in(numCourses, 0);
+        
+        for (auto a : prerequisites) {
+            graph[a.second].push_back(a.first);
+            ++in[a.first];
+        }
+        
+        queue<int> q;
+        for (int i = 0; i < numCourses; ++i) {
+            if (in[i] == 0) q.push(i);
+        }
+        
+        while (!q.empty()) {
+            int t = q.front();
+            q.pop();
+            for (auto a : graph[t]) {
+                --in[a];
+                if (in[a] == 0) q.push(a);
+            }
+        }
+        
+        for (int i = 0; i < numCourses; ++i) {
+            if (in[i] != 0) return false;
+        }
+        
+        return true;
+        
+    }
+
 /* bfs , 2016-06-23 update */
 
     bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
