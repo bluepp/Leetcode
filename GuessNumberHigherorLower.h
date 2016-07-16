ecd19cee68f874b3a2494d152ead4b1dcb1f5910
bluepp <bluepp@gmail.com>
2016-07-17 01:32:25 +0800

    Create GuessNumberHigherorLower.h

---
+++ GuessNumberHigherorLower.h
@@ -0,0 +1,49 @@
+/*
+bluepp
+2016-07-17
+May the force be with me!
+
+https://leetcode.com/problems/guess-number-higher-or-lower/
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I'll tell you whether the number is higher or lower.
+
+You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+
+-1 : My number is lower
+ 1 : My number is higher
+ 0 : Congrats! You got it!
+Example:
+n = 10, I pick 6.
+
+Return 6.
+*/
+
+    int guessNumber(int n) {
+        
+        int l = 1, r = n;
+        
+        while (l < r)
+        {
+            int m = l + (r-l)/2;
+            
+            if (guess(m) == 0)
+            {
+                return m;
+            }
+            else if (guess(m) < 0)
+            {
+                r = m-1;
+            }
+            else
+            {
+                l = m+1;
+            }
+        }
+        
+        return l;
+        
+    }
