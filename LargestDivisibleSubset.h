a5e04ad85ba4af877fbfb1e12b07df3735e74898
bluepp <bluepp@gmail.com>
2016-07-18 23:29:00 +0800

    Update LargestDivisibleSubset.h

--- LargestDivisibleSubset.h
+++ LargestDivisibleSubset.h
@@ -22,6 +22,47 @@ Result: [1,2,4,8]
 
 */
 
+/* 2016-07-18, update */
+
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        vector<int> dp(n, 0), parent(n, 0);
+        vector<int> res;
+        
+        int mx = 0, mx_idx =0;
+        
+        for (int i = n-1; i >= 0; i--)
+        {
+            for (int j = i; j < n; j++)
+            {
+                if (nums[j] % nums[i] == 0 && dp[i] < dp[j] + 1)
+                {
+                    dp[i] = dp[j] + 1;
+                    parent[i] = j;
+                    
+                    if (mx < dp[i])
+                    {
+                        mx = dp[i];
+                        mx_idx = i;
+                    }
+                }
+            }
+        }
+    
+        for (int i = 0; i < mx; ++i) {
+            res.push_back(nums[mx_idx]);
+            mx_idx = parent[mx_idx];
+        }
+        
+        return res;
+    }
+
+
+
+
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         
         vector<int> res;
