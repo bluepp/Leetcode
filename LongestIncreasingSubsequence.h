b0451e7fb17dfc281575aa91a4a17263fbfefaa0
bluepp <bluepp@gmail.com>
2016-07-25 08:09:42 +0800

    Create LongestIncreasingSubsequence.h

---
+++ LongestIncreasingSubsequence.h
@@ -0,0 +1,37 @@
+/*
+2016-07-25
+bluepp
+May the force be with me!
+
+https://leetcode.com/problems/longest-increasing-subsequence/
+
+Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+For example,
+Given [10, 9, 2, 5, 3, 7, 101, 18],
+The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. Note that there may be more than one LIS combination, it is only necessary for you to return the length.
+
+Your algorithm should run in O(n2) complexity.
+
+Follow up: Could you improve it to O(n log n) time complexity?
+*/
+
+    int lengthOfLIS(vector<int>& nums) {
+        
+       if (nums.empty()) return 0;
+        vector<int> ends{nums[0]};
+        for (auto a : nums) {
+            if (a < ends[0]) ends[0] = a;
+            else if (a > ends.back()) ends.push_back(a);
+            else {
+                int left = 0, right = ends.size();
+                while (left < right) {
+                    int mid = left + (right - left) / 2;
+                    if (ends[mid] < a) left = mid + 1;
+                    else right = mid;
+                }
+                ends[right] = a;
+            }
+        }
+        return ends.size();
+    }
