3b6d36193e2c0c927e18b12cd7b1a90828d53065
bluepp <bluepp@gmail.com>
2016-07-25 10:10:52 +0800

    Update LongestIncreasingSubsequence.h

--- LongestIncreasingSubsequence.h
+++ LongestIncreasingSubsequence.h
@@ -18,20 +18,35 @@ Follow up: Could you improve it to O(n log n) time complexity?
 
     int lengthOfLIS(vector<int>& nums) {
         
-       if (nums.empty()) return 0;
-        vector<int> ends{nums[0]};
-        for (auto a : nums) {
-            if (a < ends[0]) ends[0] = a;
-            else if (a > ends.back()) ends.push_back(a);
-            else {
-                int left = 0, right = ends.size();
-                while (left < right) {
-                    int mid = left + (right - left) / 2;
-                    if (ends[mid] < a) left = mid + 1;
-                    else right = mid;
+        vector<int> dp;
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int l = 0, r = dp.size();
+            
+            while (l < r)
+            {
+                int m = l + (r-l)/2;
+                
+                if (dp[m] < nums[i])
+                {
+                    l = m+1;
                 }
-                ends[right] = a;
+                else
+                {
+                    r = m;
+                }
+            }
+            
+            if (r >= dp.size())
+            {
+                dp.push_back(nums[i]);
+            }
+            else
+            {
+                dp[r] = nums[i];
             }
         }
-        return ends.size();
+        
+        return dp.size();
     }
