35d8ec8fde1f08460d27b994a4ae92fe95816013
bluepp <bluepp@gmail.com>
2016-08-29 15:12:37 +0800

    Update RotateList.h

--- RotateList.h
+++ RotateList.h
@@ -18,6 +18,32 @@
            This solution traverses the list twice at most.
 */
 
+/* 2016-08-29, update */
+
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+        if (!head) return NULL;
+        int n = 1;
+        
+        ListNode *cur = head;
+        while (cur->next) {
+            ++n;
+            cur = cur->next;
+        }
+        
+        cur->next = head;
+        int m = n - k % n;
+        for (int i = 0; i < m; ++i) {
+            cur = cur->next;
+        }
+        
+        ListNode *newhead = cur->next;
+        cur->next = NULL;
+        
+        return newhead;
+    }
+
+
     ListNode *rotateRight(ListNode *head, int k) {
         if (!head) return NULL;
         
