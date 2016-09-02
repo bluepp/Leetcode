2be90b239676548dcfc8b6a26b1bf400af45fba5
bluepp <bluepp@gmail.com>
2016-09-02 17:02:46 +0800

    Update UniquePathsII.h

--- UniquePathsII.h
+++ UniquePathsII.h
@@ -26,6 +26,44 @@
 
  Solution: Dynamic programming.
  */
+ 
+/* 2016-09-02. update */
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+        if (obstacleGrid.empty() || obstacleGrid[0].empty())
+        {
+            return 0;
+        }
+        
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        dp[m-1][n-1] = (obstacleGrid[m-1][n-1] == 1) ? 0 : 1;
+        
+        for (int i = m-2; i >= 0; i--)
+        {
+            dp[i][n-1] = (obstacleGrid[i][n-1] == 1) ? 0 : dp[i+1][n-1];
+
+        }
+        
+        for (int j = n-2; j >= 0; j--)
+        {
+            dp[m-1][j] = obstacleGrid[m-1][j] == 1 ? 0 : dp[m-1][j+1];
+        }
+        
+        for (int i = m-2; i >= 0; i--)
+        {
+            for (int j = n-2; j >= 0; j--)
+            {
+                dp[i][j] = obstacleGrid[i][j] ? 0 : (dp[i+1][j]+dp[i][j+1]);
+            }
+        }
+        
+        return dp[0][0];
+        
+    }
+
 
 /* My solution */
 
