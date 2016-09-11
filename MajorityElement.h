5724fc7913b41df4c2ecf36c54cceba4eef2ca2c
bluepp <bluepp@gmail.com>
2016-09-11 23:39:54 +0800

    Update MajorityElement.h

--- MajorityElement.h
+++ MajorityElement.h
@@ -9,6 +9,35 @@ Given an array of size n, find the majority element. The majority element is the
 You may assume that the array is non-empty and the majority element always exist in the array.
 */
 
+/* 2016-09-11, update */
+    int majorityElement(vector<int>& nums) {
+        
+        int ret = 0;
+        int count = 0;
+        
+        for (auto p : nums)
+        {
+            if (p == ret)
+            {
+                count++;
+            }
+            else if (count == 0) 
+            {
+                ret = p;
+                count = 1;
+            }
+            else
+            {
+                count --;
+            }
+        }
+        
+        return ret;
+      
+    }
+
+
+
     int majorityElement(vector<int> &num) {
         int n = num.size();
         
