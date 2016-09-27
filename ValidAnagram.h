1d85e3374fcd67ef08bb837dbaf42111fc916911
bluepp <bluepp@gmail.com>
2016-09-27 16:41:26 +0800

    Update ValidAnagram.h

--- ValidAnagram.h
+++ ValidAnagram.h
@@ -15,6 +15,39 @@ You may assume the string contains only lowercase alphabets.
 https://leetcode.com/problems/valid-anagram/
 */
 
+/* 2016-09-27, update */
+
+    bool isAnagram(string s, string t) {
+        
+        unordered_map<char, int> map;
+        
+        for (auto p : s)
+        {
+            map[p]++;
+        }
+        
+        for (auto p : t)
+        {
+            if (!map.count(p))
+            {
+                return false;
+            }
+            else
+            {
+                map[p]--;
+                if (map[p] == 0)
+                {
+                    map.erase(p);
+                }
+            }
+        }
+        
+        return map.empty();
+    }
+
+
+
+
     bool isAnagram(string s, string t) {
         int n1 = s.length(), n2 = t.length();
         if (n1 != n2) return false;
