0e775057314a152d820f61fa85a3f733431feb40
bluepp <bluepp@gmail.com>
2016-10-01 07:41:02 +0800

    Update SortList.h

--- SortList.h
+++ SortList.h
@@ -17,6 +17,65 @@
 
  Solution: merge sort.
  */
+
+/* 2016-10-01, update */
+
+    ListNode* sortList(ListNode* head) {
+        
+        if (!head || !head->next) return head;
+        ListNode *fast = head, *slow = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        fast = slow;
+        slow = slow->next;
+        fast->next = NULL;
+        fast = sortList(head);
+        slow = sortList(slow);
+        return merge(fast, slow);
+    }
+    
+    
+    ListNode* merge(ListNode *l1, ListNode *l2)
+    {
+        ListNode dummy(0), *prev = &dummy;
+        
+        while (l1 && l2)
+        {
+            if (l1->val < l2->val)
+            {
+                prev->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                prev->next = l2;
+                l2 = l2->next;
+            }
+            
+            prev = prev->next;
+        }
+        
+        if (l1)
+        {
+            prev->next = l1;
+        }
+        
+        if (l2)
+        {
+            prev->next = l2;
+        }
+        
+        return dummy.next;
+        
+    }
+
+
+
+
+
+
  
  ListNode *sortList(ListNode *head) {
         return _sortList(head, getlength(head));
