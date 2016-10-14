47230e9118397a406f33259d3d3061b80f7be499
bluepp <bluepp@gmail.com>
2016-10-15 03:55:37 +0800

    Update SmallestRectangleEnclosing.h

--- SmallestRectangleEnclosing.h
+++ SmallestRectangleEnclosing.h
@@ -3,7 +3,10 @@ bluepp
 2016-06-03
 May the force be with me!
 
-An image is represented by a binary matrix with 0 as a white pixel and 1 as a black pixel. The black pixels are connected, i.e., there is only one black region. Pixels are connected horizontally and vertically. Given the location (x, y) of one of the black pixels, return the area of the smallest (axis-aligned) rectangle that encloses all black pixels.
+An image is represented by a binary matrix with 0 as a white pixel and 1 as a black pixel. 
+The black pixels are connected, i.e., there is only one black region. 
+Pixels are connected horizontally and vertically. Given the location (x, y) of one of the black pixels,
+return the area of the smallest (axis-aligned) rectangle that encloses all black pixels.
 
 For example, given the following image:
 
@@ -46,3 +49,31 @@ Return 6.
         
         return h*w;
     }
+
+
+/* 2016-10-15, update */
+
+    int minArea(vector<vector<char>>& image, int x, int y) {
+        
+        int left = y, right = y, up = x, down = x;
+        
+        dfs(image, x, y, left, right, up, down);
+        
+        return (right-left+1) *(down-up+1);
+    }
+    
+    void dfs(vector<vector<char>> &image, int x, int y, int &left, int &right, int &up, int &down) {
+        
+        if (x < 0 || x >= image.size() || y < 0 || y >= image[0].size() || image[x][y] != '1') return;
+        left = min(left, y); 
+        right = max(right, y);
+        up = min(up, x);
+        down = max(down, x);
+        image[x][y] = 'x';
+        dfs(image, x + 1, y, left, right, up, down);
+        dfs(image, x - 1, y, left, right, up, down);
+        dfs(image, x, y + 1, left, right, up, down);
+        dfs(image, x, y - 1, left, right, up, down);
+    }
+
+
