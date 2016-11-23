23e2b394054b4b2b8967152d5ffaf313b90173e5
bluepp <bluepp@gmail.com>
2016-11-23 09:36:07 +0800

    Create AddStrings.h

---
+++ AddStrings.h
@@ -0,0 +1,34 @@
+/*
+bluepp
+2013-11-23
+May the force be with me!
+
+https://leetcode.com/problems/add-strings/
+
+Given two non-negative numbers num1 and num2 represented as string, return the sum of num1 and num2.
+
+Note:
+
+The length of both num1 and num2 is < 5100.
+Both num1 and num2 contains only digits 0-9.
+Both num1 and num2 does not contain any leading zero.
+You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+*/
+
+    string addStrings(string num1, string num2) {
+        
+        string res = "";
+        int m = num1.size(), n = num2.size(), i = m - 1, j = n - 1, carry = 0;
+        
+        while (i >= 0 || j >= 0) {
+            int a = i >= 0 ? num1[i--] - '0' : 0;
+            int b = j >= 0 ? num2[j--] - '0' : 0;
+            int sum = a + b + carry;
+            res.insert(res.begin(), sum % 10 + '0');
+            carry = sum / 10;
+        }
+        
+        return carry ? "1" + res : res;
+  
+    }
