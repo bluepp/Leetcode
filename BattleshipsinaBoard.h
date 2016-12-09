e8ed3fdce9f324151b9ca9cf6be18768acf467c5
bluepp <bluepp@gmail.com>
2016-12-09 12:55:15 +0800

    Create BattleshipsinaBoard.h

---
+++ BattleshipsinaBoard.h
@@ -0,0 +1,46 @@
+/*
+bluepp
+2016-12-09
+May the force be with me!
+
+https://leetcode.com/problems/battleships-in-a-board/
+
+Given an 2D board, count how many different battleships are in it. The battleships are represented with 'X's, empty slots are represented with '.'s. You may assume the following rules:
+
+You receive a valid board, made of only battleships or empty slots.
+Battleships can only be placed horizontally or vertically. In other words, they can only be made of the shape 1xN (1 row, N columns) or Nx1 (N rows, 1 column), where N can be of any size.
+At least one horizontal or vertical cell separates between two battleships - there are no adjacent battleships.
+Example:
+X..X
+...X
+...X
+In the above board there are 2 battleships.
+Invalid Example:
+...X
+XXXX
+...X
+This is an invalid board that you will not receive - as battleships will always have a cell separating between them.
+Follow up:
+Could you do it in one-pass, using only O(1) extra memory and without modifying the value of the board?
+
+Subscribe to see which companies asked this question
+
+*/
+
+    int countBattleships(vector<vector<char>>& board) {
+        
+        if (board.empty()) return 0;
+        int m = board.size(), n = board[0].size();
+        
+        int ret = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (board[i][j] == '.' || i > 0 && board[i-1][j] == 'X' || j > 0 && board[i][j-1] == 'X') continue;
+                ret++;
+            }
+        }
+        
+        return ret;
+    }
