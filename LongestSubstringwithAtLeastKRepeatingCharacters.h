0c18751a34b3b89a6461c85a314a86e08113a6a1
bluepp <bluepp@gmail.com>
2016-12-11 23:49:58 +0800

    Create LongestSubstringwithAtLeastKRepeatingCharacters.h

---
+++ LongestSubstringwithAtLeastKRepeatingCharacters.h
@@ -0,0 +1,49 @@
+/*
+bluepp
+2016-12-11
+May the force be with me!
+
+https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/
+
+Find the length of the longest substring T of a given string (consists of lowercase letters only) such that every character in T appears no less than k times.
+
+Example 1:
+
+Input:
+s = "aaabb", k = 3
+
+Output:
+3
+
+The longest substring is "aaa", as 'a' is repeated 3 times.
+Example 2:
+
+Input:
+s = "ababbc", k = 2
+
+Output:
+5
+
+The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
+*/
+
+   int longestSubstring(string s, int k) {
+        int res = 0, i = 0, n = s.size();
+        
+        while (i + k < n) {
+            int m[26] = {0}, mask = 0, max_idx = i;
+            for (int j = i; j < n; ++j) {
+                int t = s[j] - 'a';
+                ++m[t];
+                if (m[t] < k) mask |= (1 << t);
+                else mask &= (~(1 << t));
+                if (mask == 0) {
+                    res = max(res, j - i + 1);
+                    max_idx = j;
+                }
+            }
+            i = max_idx + 1;
+        }
+        return res;
+        
+    }
