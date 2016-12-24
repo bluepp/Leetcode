cdb2c8ed19c04d5108e9886dbff638592ae9d32f
bluepp <bluepp@gmail.com>
2016-12-24 18:43:27 +0800

    Create HammingDistance.h

---
+++ HammingDistance.h
@@ -0,0 +1,40 @@
+/*
+2016-12-24
+bluepp
+May the force be with me!
+
+https://leetcode.com/problems/hamming-distance/
+
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different.
+*/
+
+    int hammingDistance(int x, int y) {
+        
+        int ret = 0;
+        int exc = x ^ y;
+        
+        for (int i = 0; i < 32; i++)
+        {
+            ret += (exc >> i) & 1;
+        }
+        
+        return ret;
+    }
