d3f514c48bf174a378e37af2296904adb9148e55
bluepp <bluepp@gmail.com>
2016-12-30 16:06:13 +0800

    Create FindAllNumbersDisappearedinanArray.h

---
+++ FindAllNumbersDisappearedinanArray.h
@@ -0,0 +1,45 @@
+/*
+bluepp
+2016-12-30
+May the force be with me!
+
+https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements of [1, n] inclusive that do not appear in this array.
+
+Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+Example:
+
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[5,6]
+*/
+
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        
+        vector<int> res;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != nums[nums[i]-1])
+            {
+                swap(nums[i], nums[nums[i]-1]);
+                i--;
+            }
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != i+1) res.push_back(i+1);
+        }
+        
+        return res;
+        
+    }
+    
