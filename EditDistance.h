c693d41e54acd54966e4f2d2d347bdaec21ca1f9
bluepp <bluepp@gmail.com>
2017-01-03 04:16:47 +0800

    Update EditDistance.h

--- EditDistance.h
+++ EditDistance.h
@@ -24,27 +24,33 @@
                2. Time: O(mn) Space: O(n);
 */
 
-    int minDistance(string word1, string word2) {
-        int m = word1.size(), n = word2.size();
-        int dp[n+1][m+1];
-        for (int j = 0; j <= m; j++)
-            dp[0][j] = j;
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = i;
+
+/* 2017-01-03, update */
+   int minDistance(string word1, string word2) {
         
-        for (int i = 1; i <= n; i++)
+        int m = word1.length(), n = word2.length();
+        
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        
+        for (int i = 0; i <= m; i++) dp[i][0] = i;
+        for (int j = 0; j <= n; j++) dp[0][j] = j;
+        
+        for (int i = 1; i <= m; i++)
         {
-            for (int j = 1; j <= m; j++)
+            for(int j = 1; j <= n; j++)
             {
-                if (word1[j-1] == word2[i-1])
+                if (word1[i-1] == word2[j-1])
+                {
                     dp[i][j] = dp[i-1][j-1];
+                }
                 else
+                {
                     dp[i][j] = min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j])) + 1;
+                }
             }
         }
         
-        return dp[n][m];
-        
+        return dp[m][n];
     }
     
     
