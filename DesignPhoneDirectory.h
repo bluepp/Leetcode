0593faf321e0b5ef9a2daeed42b5556dab9f35ab
bluepp <bluepp@gmail.com>
2017-01-02 17:41:38 +0800

    Update DesignPhoneDirectory.h

--- DesignPhoneDirectory.h
+++ DesignPhoneDirectory.h
@@ -38,48 +38,58 @@ directory.check(2);
 
 */
 
+/* 2017-01-02, update */
+
 class PhoneDirectory {
 private:
-    int max_num, next, idx;
+    int max_num, idx, next;
     vector<int> recycle, flag;
     
 public:
     /** Initialize your data structure here
         @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
     PhoneDirectory(int maxNumbers) {
+        
         max_num = maxNumbers;
-        next = idx = 0;
+        idx = next = 0;
+        
         recycle.resize(max_num);
-        flag.resize(max_num, 1);;
+        flag.resize(max_num, 1);
     }
     
     /** Provide a number which is not assigned to anyone.
         @return - Return an available number. Return -1 if none is available. */
     int get() {
+        
         if (next == max_num && idx <= 0) return -1;
         if (idx > 0) {
             int t = recycle[--idx];
             flag[t] = 0;
             return t;
         }
-        flag[next] = false;
-        return next++;
+        else
+        {
+            flag[next] = 0;
+            return next++;
+        }
+        
     }
     
     /** Check if a number is available or not. */
     bool check(int number) {
         
         return number >= 0 && number < max_num && flag[number];
-        
+    
     }
     
     /** Recycle or release a number. */
     void release(int number) {
         
-        if (number >= 0 && number < max_num && !flag[number]) {
+        if (number >= 0 && number < max_num && !flag[number])
+        {
             recycle[idx++] = number;
             flag[number] = 1;
         }
-        
+   
     }
 };
