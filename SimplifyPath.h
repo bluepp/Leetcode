e72a729e94600153b2a135ad29e58d099da66a5f
bluepp <bluepp@gmail.com>
2017-01-03 18:07:07 +0800

    Update SimplifyPath.h

--- SimplifyPath.h
+++ SimplifyPath.h
@@ -30,6 +30,44 @@
  */
  
  
+/* 2017-01-03, update */
+   string simplifyPath(string path) {
+        vector<string> v;
+        int i = 0;
+        
+        while (i < path.size()) {
+            
+            while (path[i] == '/' && i < path.size()) ++i;
+            
+            if (i == path.size()) break;
+            
+            int start = i;
+            while (path[i] != '/' && i < path.size()) ++i;
+            int end = i - 1;
+            
+            string s = path.substr(start, end - start + 1);
+            
+            if (s == "..") {
+                if (!v.empty()) v.pop_back(); 
+            } else if (s != ".") {
+                v.push_back(s);
+            }
+        }
+        
+        if (v.empty()) return "/";
+        
+        string res;
+        for (int i = 0; i < v.size(); ++i) {
+            res += '/' + v[i];
+        }
+        
+        return res;
+        
+    }
+
+
+
+
  /* 2014-12-09 */
  /*http://bangbingsyb.blogspot.com/search?q=simplify+path */
      string simplifyPath(string path) {
