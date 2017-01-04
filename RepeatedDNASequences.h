893c953f9c5d8c4058ee5c9be28135ef4ef98570
bluepp <bluepp@gmail.com>
2017-01-05 00:46:53 +0800

    Update RepeatedDNASequences.h

--- RepeatedDNASequences.h
+++ RepeatedDNASequences.h
@@ -36,6 +36,29 @@ https://leetcode.com/problems/repeated-dna-sequences/
     }
 
 
+/*2015-01-07, update */
+   vector<string> findRepeatedDnaSequences(string s) {
+        
+        unordered_set<string> res;
+        unordered_set<int> st;
+        unordered_map<int, int> m{{'A', 0}, {'C', 1}, {'G', 2}, {'T', 3}};
+        
+        int cur = 0, i = 0;
+        while (i < 9) cur = cur << 2 | m[s[i++]];
+        
+        while (i < s.size()) {
+            
+            cur = ((cur & 0x3ffff) << 2) | (m[s[i++]]);
+            if (st.count(cur)) res.insert(s.substr(i - 10, 10));
+            else st.insert(cur);
+        }
+        
+        return vector<string>(res.begin(), res.end());
+        
+    }
+
+
+
 
 /* 2016-07-19, update */
 /* 2016-08-01, minor update, easy understanding */
