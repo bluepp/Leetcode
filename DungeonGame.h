466c926241c877fa2121d8b1166b754cf1a3cf9b
bluepp <bluepp@gmail.com>
2017-01-07 01:07:59 +0800

    Update DungeonGame.h

--- DungeonGame.h
+++ DungeonGame.h
@@ -26,24 +26,30 @@ https://leetcode.com/problems/dungeon-game/
 /* http://www.fgdsb.com/2015/01/06/harry-potter-game/ */
 /* dp[i][j] = max(1, min(dp[i+1][j], dp[i][j+1]) - dungeon[i][j]) */
 
-     int calculateMinimumHP(vector<vector<int>>& dungeon) {
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
         
-        int m = dungeon.size();
-        int n = dungeon[0].size();
-        int dp[m][n];
-        dp[m - 1][n - 1] = max(1, 1 - dungeon[m - 1][n - 1]);
-        for (int i = m - 2; i >= 0; --i) {
-            dp[i][n - 1] = max(1, dp[i + 1][n - 1] - dungeon[i][n - 1]);
+        int m = dungeon.size(), n = dungeon[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        dp[m-1][n-1] = max(1, 1-dungeon[m-1][n-1]);
+        
+        for (int i = m-2; i >= 0; i--)
+        {
+            dp[i][n-1] = max(1, dp[i+1][n-1]-dungeon[i][n-1]);
         }
-        for (int j = n - 2; j >= 0; --j) {
-            dp[m - 1][j] = max(1, dp[m - 1][j + 1] - dungeon[m - 1][j]);
+        
+        for (int j = n-2; j >= 0; j--)
+        {
+            dp[m-1][j] = max(1, dp[m-1][j+1]- dungeon[m-1][j]);
         }
-        for (int i = m - 2; i >= 0; --i) {
-            for (int j = n - 2; j >= 0; --j) {
-                dp[i][j] = max(1, min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]);
+        
+        for (int i = m-2; i >= 0; i--)
+        {
+            for (int j = n-2; j >= 0; j--)
+            {
+                dp[i][j] = max(1, min(dp[i+1][j], dp[i][j+1])-dungeon[i][j]);
             }
         }
-        return dp[0][0];
- 
         
+        return dp[0][0];
     }
