734dbbbe3ffa18b4dcfe424eb4b99fef8b756b6e
bluepp <bluepp@gmail.com>
2017-01-06 19:29:36 +0800

    Update CountOfRangeSum.h

--- CountOfRangeSum.h
+++ CountOfRangeSum.h
@@ -15,4 +15,19 @@ Return 3.
 The three ranges are : [0, 0], [2, 2], [0, 2] and their respective sums are: -2, -1, 2.
 */
 
-/* http://huntzhan.org/leetcode-count-of-range-sum/ */
+    int countRangeSum(vector<int>& nums, int lower, int upper) {
+        
+        int res = 0;
+        long long sum = 0;
+        multiset<long long> sums;
+        sums.insert(0);
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            
+            sum += nums[i];
+            res += distance(sums.lower_bound(sum - upper), sums.upper_bound(sum - lower));
+            sums.insert(sum);
+        }
+        
+        return res;
+    }
