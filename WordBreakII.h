e03f6c5683669b091cc43d3a085b248284932456
bluepp <bluepp@gmail.com>
2017-01-08 17:02:16 +0800

    Update WordBreakII.h

--- WordBreakII.h
+++ WordBreakII.h
@@ -74,4 +74,44 @@ vector<string> wordBreak(string s, unordered_set<string> &dict) {
         
         return dp[0];
     }
+
+
+
+
+/* 2017-01-08, uodate, TLE ? */
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        
+        vector<string> res;
+        if (wordDict.empty() || wordDict[0].empty()) return res;
+        
+        unordered_map<string, int> map;
+        for (auto str : wordDict)
+        {
+            map[str]++;
+        }
+        
+        _dfs(s, map, 0, "", res);
+        return res;
+    }
+    
+    void _dfs(string s, unordered_map<string, int> map, int start, string str, vector<string> &res)
+    {
+        if (start == s.length())
+        {
+            res.push_back(str);
+            return;
+        }
+        
+        if (!str.empty()) str += ' ';
+        
+        for (int i = start; i < s.length(); i++)
+        {
+            string t = s.substr(start, i-start+1);
+            
+            if (map.count(t))
+            {
+                _dfs(s, map, i+1, str+t, res);
+            }
+        }
+    }
     
