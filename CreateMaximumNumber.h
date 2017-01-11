8adedcce57e427061b91f21e5ec935015d0ff2cd
bluepp <bluepp@gmail.com>
2017-01-11 17:21:38 +0800

    Create CreateMaximumNumber.h

---
+++ CreateMaximumNumber.h
@@ -0,0 +1,65 @@
+/*
+bluepp
+2017-01-11
+May the force be with me!
+
+https://leetcode.com/problems/create-maximum-number/
+
+Given two arrays of length m and n with digits 0-9 representing two numbers. Create the maximum number of length k <= m + n from digits of the two. The relative order of the digits from the same array must be preserved. Return an array of the k digits. You should try to optimize your time and space complexity.
+
+Example 1:
+nums1 = [3, 4, 6, 5]
+nums2 = [9, 1, 2, 5, 8, 3]
+k = 5
+return [9, 8, 6, 5, 3]
+
+Example 2:
+nums1 = [6, 7]
+nums2 = [6, 0, 4]
+k = 5
+return [6, 7, 6, 0, 4]
+
+Example 3:
+nums1 = [3, 9]
+nums2 = [8, 9]
+k = 3
+return [9, 8, 9]
+*/
+
+    vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
+        
+        int m = nums1.size(), n = nums2.size();
+        vector<int> res;
+        
+        for (int i = max(0, k - n); i <= min(k, m); ++i) {
+            res = max(res, mergeVector(maxVector(nums1, i), maxVector(nums2, k - i)));
+        }
+        
+        return res;
+        
+    }
+    
+    vector<int> maxVector(vector<int> nums, int k) {
+        
+        int drop = nums.size() - k;
+        vector<int> res;
+        for (int num : nums) {
+            while (drop && res.size() && res.back() < num) {
+                res.pop_back();
+                --drop;
+            }
+            res.push_back(num);
+        }
+        res.resize(k);
+        return res;
+    }
+    
+    vector<int> mergeVector(vector<int> nums1, vector<int> nums2) {
+        vector<int> res;
+        while (nums1.size() + nums2.size()) {
+            vector<int> &tmp = nums1 > nums2 ? nums1 : nums2;
+            res.push_back(tmp[0]);
+            tmp.erase(tmp.begin());
+        }
+        return res;
+    }
