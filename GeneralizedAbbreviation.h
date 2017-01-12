a4a6b448b674f41a32c41686c48f0216254abc3b
bluepp <bluepp@gmail.com>
2017-01-12 21:19:26 +0800

    Update GeneralizedAbbreviation.h

--- GeneralizedAbbreviation.h
+++ GeneralizedAbbreviation.h
@@ -54,6 +54,23 @@ Given word = "word", return the following list (order does not matter):
     }
 
 
+/* recursion */
+
+    vector<string> generateAbbreviations(string word) {
+        vector<string> res;
+        helper(word, 0, 0, "", res);
+        return res;
+    }
+    void helper(string word, int pos, int cnt, string out, vector<string> &res) {
+        if (pos == word.size()) {
+            if (cnt > 0) out += to_string(cnt);
+            res.push_back(out);
+        } else {
+            helper(word, pos + 1, cnt + 1, out, res);
+            helper(word, pos + 1, 0, out + (cnt > 0 ? to_string(cnt) : "") + word[pos], res);
+        }
+    }
+
 
 /* straight forward recursion soluation */
 
