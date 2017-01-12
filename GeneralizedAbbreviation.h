b819a2e992a62dac88608f1fea6e38573bc3dbd0
bluepp <bluepp@gmail.com>
2017-01-12 19:39:38 +0800

    Update GeneralizedAbbreviation.h

--- GeneralizedAbbreviation.h
+++ GeneralizedAbbreviation.h
@@ -11,44 +11,46 @@ Given word = "word", return the following list (order does not matter):
 ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
 */
 
-/* 2016-07-07, iteration version */
-
+/* 2017-01-12, iterative, update */
     vector<string> generateAbbreviations(string word) {
         
         vector<string> res;
+        int n = word.length();
         
-        for (int i = 0; i < pow(2, word.size()); i ++)
+        for (int i = 0; i < pow(2, n); i++)
         {
-            string out = "";
+            string str;
             int count = 0;
+            int t = i;
             
-            for (int j = 0; j < word.size(); j++)
+            for (int j = 0; j < n; j++)
             {
-                if ((i >> j) & 1)
+                if (t & 1 == 1)
                 {
-                    count ++;
+                    count++;
+                    
+                    if (j == n-1) str += to_string(count);
                 }
                 else
                 {
-                    if (count != 0)
+                    if (count)
                     {
-                        out += to_string(count);
+                        str += to_string(count);
                         count = 0;
                     }
                     
-                    out += word[j];
+                    str += word[j];
                 }
+                
+                t >>= 1;
             }
             
-            if (count > 0)
-            {
-                out += to_string(count);
-            }
+            res.push_back(str);
             
-            res.push_back(out);
         }
         
         return res;
+       
     }
 
 
