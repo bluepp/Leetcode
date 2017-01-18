61a5fc09c726291dbf94f5ce51121ddbd87e03ac
bluepp <bluepp@gmail.com>
2017-01-19 01:48:24 +0800

    Update PalindromePairs.h

--- PalindromePairs.h
+++ PalindromePairs.h
@@ -17,6 +17,59 @@ Return [[0, 1], [1, 0], [3, 2], [2, 4]]
 The palindromes are ["dcbaabcd", "abcddcba", "slls", "llssssll"]
 */
 
+/* 2017-01-19, update */
+
+    vector<vector<int>> palindromePairs(vector<string>& words) {
+        
+        vector<vector<int>> res;
+        unordered_map<string, int> m;
+        set<int> s;
+        
+        for (int i = 0; i < words.size(); ++i) 
+        {
+            m[words[i]] = i;
+            s.insert(words[i].size());
+        }
+        
+        for (int i = 0; i < words.size(); ++i) 
+        {
+            string t = words[i];
+            int len = t.size();
+            reverse(t.begin(), t.end());
+            
+            if (m.count(t) && m[t] != i) 
+            {
+                res.push_back({i, m[t]});
+            }
+            
+            auto a = s.find(len);
+            for (auto it = s.begin(); it != a; ++it) 
+            {
+                int d = *it;
+                
+                if (isValid(t, 0, len - d - 1) && m.count(t.substr(len - d))) {
+                    res.push_back({i, m[t.substr(len - d)]});
+                }
+                if (isValid(t, d, len - 1) && m.count(t.substr(0, d))) {
+                    res.push_back({m[t.substr(0, d)], i});
+                }
+            }
+        }
+        
+        return res;
+    }
+    
+    bool isValid(string t, int l, int r)
+    {
+        while (l < r)
+        {
+            if (t[l++] != t[r--]) return false;
+        }
+        
+        return true;
+    }
+
+
 
    vector<vector<int>> palindromePairs(vector<string>& words) {
         
