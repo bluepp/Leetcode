7a9fee6f73430de2cf527c440c9d3ac3361b13d1
bluepp <bluepp@gmail.com>
2017-02-13 02:17:52 +0800

    Update InterleavingString.h

--- InterleavingString.h
+++ InterleavingString.h
@@ -23,24 +23,36 @@
               2. Recursion. Okay for Small but TLE for Large Test data.
  */
  
+
+/* 2017-02-13, update */
     bool isInterleave(string s1, string s2, string s3) {
-        int m = s1.size(), n = s2.size(), k = s3.size();
-        if (m + n != k) return false;
         
-        bool dp[n+1][m+1];
-        dp[0][0] = true;
-        for (int i = 1; i <= n; ++i)
-            dp[i][0] = dp[i-1][0] && s2[i-1] == s3[i-1];
-        for (int j = 1; j <= m; ++j)
-            dp[0][j] = dp[0][j-1] && s1[j-1] == s3[j-1];
-
-        for (int i = 1; i <= n; ++i)
-            for (int j = 1; j <= m; ++j)
-                dp[i][j] = dp[i-1][j] && s2[i-1] == s3[i+j-1] ||
-                           dp[i][j-1] && s1[j-1] == s3[i+j-1];
-
-        return dp[n][m];
-
+        int n1 = s1.length(), n2 = s2.length(), n3 = s3.length();
+        if (n1+n2 != n3) return false;
+        
+        vector<vector<bool>> dp(n1+1, vector<bool>(n2+1, false));
+        dp[0][0]= true;
+        
+        for (int i = 1; i <= n1; i++)
+        {
+            dp[i][0] = dp[i-1][0] && (s1[i-1] == s3[i-1]);
+        }
+        
+        for (int j = 1; j <= n2; j++)
+        {
+            dp[0][j] = dp[0][j-1] && (s2[j-1] == s3[j-1]);
+        }
+        
+        for (int i = 1; i <= n1; i++)
+        {
+            for (int j = 1; j <= n2; j++)
+            {
+                dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[i-1+j]) || (dp[i][j-1] && s2[j-1] == s3[j-1+i]);
+            }
+        }
+        
+        return dp[n1][n2];
+      
     }
     
     ------------------------------
