3675d606417272fca37ed4a6733ffd5b4aa97c43
bluepp <bluepp@gmail.com>
2017-02-12 15:47:29 +0800

    Update MiniParser.h

--- MiniParser.h
+++ MiniParser.h
@@ -51,3 +51,44 @@ Return a NestedInteger object containing a nested list with 2 elements:
         
         return res;        
     }
+
+
+/* 2017-02-12 */
+
+    NestedInteger deserialize(string s) {
+        
+        if (s.empty()) return NestedInteger();
+        if (s[0] != '[') return NestedInteger(stoi(s));
+        stack<NestedInteger> stk;
+        
+        int start = 1;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '[')
+            {
+                stk.push(NestedInteger());
+                start = i+1;
+            }
+            else if (s[i] == ',' || s[i] == ']')
+            {
+                if (i > start)
+                {
+                    stk.top().add(NestedInteger(stoi(s.substr(start, i-start))));
+                }
+                
+                start = i+1;
+                
+                if (s[i] == ']')
+                {
+                    if (stk.size() > 1)
+                    {
+                        NestedInteger t = stk.top();
+                        stk.pop();
+                        stk.top().add(t);
+                    }
+                }
+            }
+        }
+        
+        return stk.top();
+    }
