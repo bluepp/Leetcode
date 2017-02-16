6fee61282b73f8060c3a5cd0ab17b40c545e0ef2
bluepp <bluepp@gmail.com>
2017-02-16 22:30:26 +0800

    Update StrobogrammaticNumberIII.h

--- StrobogrammaticNumberIII.h
+++ StrobogrammaticNumberIII.h
@@ -15,6 +15,40 @@ Because the range might be a large number, the low and high numbers are represen
 https://leetcode.com/problems/strobogrammatic-number-iii/
 */
 
+/* 2017-02-16, update */
+
+    int strobogrammaticInRange(string low, string high) {
+        
+        int res = 0;
+        find(low, high, "", res);
+        find(low, high, "0", res);
+        find(low, high, "1", res);
+        find(low, high, "8", res);
+        
+        return res;
+    }
+    
+    void find(string low, string high, string w, int &res)
+    {
+        if (w.size() >= low.size() && w.size() <= high.size())
+        {
+            if ((w.size() == low.size() && w.compare(low) < 0) || (w.size() == high.size() && w.compare(high) > 0))
+            {
+                return;
+            }
+            if (!(w.size() > 1 && w[0] == '0')) ++res;
+        }
+        
+        if (w.size() + 2 > high.size()) return;
+        find(low, high, "0" + w + "0", res);
+        find(low, high, "1" + w + "1", res);
+        find(low, high, "6" + w + "9", res);
+        find(low, high, "8" + w + "8", res);
+        find(low, high, "9" + w + "6", res);
+    }
+
+
+
 /* MLE, http://likemyblogger.blogspot.com/2015/08/leetcode-248-strobogrammatic-number-iii.html */
     int strobogrammaticInRange(string low, string high) {
         int n = high.size();
