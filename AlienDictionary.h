f042bcea83969ba25a15cf37a309b1a8fd0ac300
bluepp <bluepp@gmail.com>
2017-02-17 16:01:35 +0800

    Update AlienDictionary.h

--- AlienDictionary.h
+++ AlienDictionary.h
@@ -27,6 +27,48 @@ https://leetcode.com/problems/alien-dictionary/
 
 */
 
+/* 2017-02-17, update */
+    string alienOrder(vector<string>& words) {
+        
+        set<pair<char, char> > s;
+        unordered_set<char> ch;
+        vector<int> in(256, 0);
+        queue<char> q;
+        string res = "";
+        for (auto a : words) ch.insert(a.begin(), a.end());
+        for (int i = 0; i < words.size() - 1; ++i) {
+            int mn = min(words[i].size(), words[i + 1].size()), j = 0;
+            for (; j < min(words[i].size(), words[i + 1].size()); ++j) {
+                if (words[i][j] != words[i + 1][j]) {
+                    s.insert(make_pair(words[i][j], words[i + 1][j]));
+                    break;
+                }
+            }
+            if (j == mn && words[i].size() > words[i + 1].size()) return "";
+        }
+        for (auto a : s) ++in[a.second];
+        for (auto a : ch) {
+            if (in[a] == 0) {
+                q.push(a);
+                res += a;
+            } 
+        }
+        while (!q.empty()) {
+            char c = q.front(); q.pop();
+            for (auto a : s) {
+                if (a.first == c) {
+                    --in[a.second];
+                    if (in[a.second] == 0) {
+                        q.push(a.second);
+                        res += a.second;
+                    }
+                }
+            }
+        }
+        return res.size() == ch.size() ? res : "";
+        
+    }
+
 /* https://leetcode.com/discuss/54188/16-18-lines-python-29-lines-c */
 
    string alienOrder(vector<string>& words) {
