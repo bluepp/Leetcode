63499bcdec5345774205485516fd741842b81836
bluepp <bluepp@gmail.com>
2017-02-23 14:34:24 +0800

    Update ImplemenQueueusingStacks.h

--- ImplemenQueueusingStacks.h
+++ ImplemenQueueusingStacks.h
@@ -17,6 +17,78 @@ You may assume that all operations are valid (for example, no pop or peek operat
 https://leetcode.com/problems/implement-queue-using-stacks/
 */
 
+/* 2017-0-23 */
+class MyQueue {
+private:
+    stack<int> s1, s2;
+    
+public:
+    /** Initialize your data structure here. */
+    MyQueue() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        
+        s1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        
+        while (s1.size() > 1)
+        {
+            int t = s1.top();
+            s1.pop();
+            s2.push(t);
+        }
+        
+        int ret = s1.top();
+        s1.pop();
+        
+        while (!s2.empty())
+        {
+            int t = s2.top();
+            s2.pop();
+            s1.push(t);
+        }
+        
+        return ret;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        
+        int ret = 0;
+        while (!s1.empty())
+        {
+            ret = s1.top();
+            s1.pop();
+            
+            s2.push(ret);
+        }
+        
+        while (!s2.empty())
+        {
+            int t = s2.top();
+            s2.pop();
+            s1.push(t);
+        }
+        
+        return ret;
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        
+        return s1.empty() && s2.empty();
+        
+    }
+};
+
+
+
 class Queue {
 private:
     stack<int> s1, s2;
