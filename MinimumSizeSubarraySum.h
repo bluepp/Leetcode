82c69c774f67ebb8a7489ed1e930dc5a9165c169
bluepp <bluepp@gmail.com>
2017-02-24 21:21:40 +0800

    Update MinimumSizeSubarraySum.h

--- MinimumSizeSubarraySum.h
+++ MinimumSizeSubarraySum.h
@@ -41,6 +41,33 @@ https://leetcode.com/problems/minimum-size-subarray-sum/
     }
 
 
+/* O(nlogn) ??? */
+
+    int minSubArrayLen(int s, vector<int>& nums) {
+        
+        int res = INT_MAX, n = nums.size();
+        vector<int> sums(n + 1, 0);
+        
+        for (int i = 1; i < n + 1; ++i) sums[i] = sums[i - 1] + nums[i - 1];
+        
+        for (int i = 0; i < n; ++i) {
+            
+            int left = i + 1, right = n, t = sums[i] + s;
+            
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (sums[mid] < t) left = mid + 1;
+                else right = mid - 1;
+            }
+            
+            if (left == n + 1) break;
+            res = min(res, left - i);
+        }
+        
+        return res == INT_MAX ? 0 : res;
+    }
+
+
     
     /* O(nlongn) http://www.cnblogs.com/grandyang/p/4501934.html */
      int minSubArrayLen(int s, vector<int>& nums) {
