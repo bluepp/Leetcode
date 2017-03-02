adde9757f068218a0431526653fd9cd37d584fe5
bluepp <bluepp@gmail.com>
2017-03-02 12:09:05 +0800

    Create MinimumAbsoluteDifferenceinBST.h

---
+++ MinimumAbsoluteDifferenceinBST.h
@@ -0,0 +1,47 @@
+/*
+bluepp
+2017-03-02
+May the force be with me!
+
+https://leetcode.com/problems/minimum-absolute-difference-in-bst/?tab=Description
+
+Given a binary search tree with non-negative values, find the minimum absolute difference between values of any two nodes.
+
+Example:
+
+Input:
+
+   1
+    \
+     3
+    /
+   2
+
+Output:
+1
+
+Explanation:
+The minimum absolute difference is 1, which is the difference between 2 and 1 (or between 2 and 3).
+Note: There are at least two nodes in this BST.
+*/
+
+    int getMinimumDifference(TreeNode* root) {
+        
+        int mindiff = INT_MAX, val = -1;
+        
+        _diff(root, val, mindiff);
+        return mindiff;
+    }
+    
+    void _diff(TreeNode* root, int &val, int &mindiff)
+    {
+        if (root->left) _diff(root->left, val, mindiff);
+        
+        if (val >= 0)
+        {
+            mindiff = min(mindiff, abs(root->val-val));
+        }
+        
+        val = root->val;
+        if (root->right) _diff(root->right, val, mindiff);
+    }
