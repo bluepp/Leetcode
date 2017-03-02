1bd02ee104497cebec0ed6c54634c5e0127b5f10
bluepp <bluepp@gmail.com>
2017-03-02 14:00:16 +0800

    Update DataStreamAsDisjointIntervals.h

--- DataStreamAsDisjointIntervals.h
+++ DataStreamAsDisjointIntervals.h
@@ -18,6 +18,67 @@ Follow up:
 What if there are lots of merges and the number of disjoint intervals are small compared to the data stream's size?
 */
 
+/* 2017-03-02, my version, update */
+
+class SummaryRanges {
+private:
+    vector<Interval> vec;
+
+
+public:
+    /** Initialize your data structure here. */
+    SummaryRanges() {
+        
+    }
+    
+    void addNum(int val) {
+        
+        vector<Interval> res;
+        
+        Interval v(val, val);
+        bool insert = false;
+        
+        for(auto p : vec)
+        {
+            if (p.start <= v.start && v.end <= p.end)
+            {
+                res.push_back(p);
+                insert = true;
+            }
+            else if (p.end <= v.start)
+            {
+                if (p.end+1 == v.start || p.end == v.start) v.start = p.start;
+                else
+                {
+                    res.push_back(p);
+                }
+            }
+            else if (v.end <= p.start)
+            {
+                if (v.end+1 == p.start || v.end == p.start) v.end = p.end;
+                else
+                {
+                    if (!insert) res.push_back(v);
+                    res.push_back(p);
+                    insert = true;
+                }
+            }
+        }
+        
+        if (!insert) res.push_back(v);
+        
+        vec = res;
+     
+    }
+    
+    vector<Interval> getIntervals() {
+        
+        return vec;
+       
+    }
+
+
+
 /* https://leetcode.com/discuss/105790/very-concise-c-solution */
 
 
