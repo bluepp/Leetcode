782425c2a05031e3fcdd57b6cb29083fb774daf3
bluepp <bluepp@gmail.com>
2017-03-07 13:06:29 +0800

    Update Non-overlappingIntervals.h

--- Non-overlappingIntervals.h
+++ Non-overlappingIntervals.h
@@ -30,40 +30,37 @@ Output: 0
 Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
 */
 
-/* 2017-02-23, my version, reasonable ? */
+/* 2017-03-07, update */
 
     int eraseOverlapIntervals(vector<Interval>& intervals) {
         
         int ret = 0;
         int n = intervals.size();
-        if (n == 0) return ret;
+        if (n == 0) return 0;
         
-        
-        sort(intervals.begin(), intervals.end(), [](Interval a, Interval b)
+        sort(intervals.begin(), intervals.end(), [&](Interval a , Interval b)
         {
             return a.start < b.start;
         });
         
-        
         Interval inter = intervals[0];
-        
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i < intervals.size(); i++)
         {
-            if (inter.end <= intervals[i].start)
+            if (intervals[i].start < inter.end)
             {
-                inter = intervals[i];
-            }
-            else 
-            {
-                if (inter.end > intervals[i].end)
+                ret ++;
+                
+                if (intervals[i].end < inter.end) 
                 {
                     inter = intervals[i];
                 }
-                
-                ret++;
             }
+            else
+            {
+                inter = intervals[i];
+            }
+            
         }
-        
         
         return ret;
       
