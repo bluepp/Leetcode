24d1b8e3c6fdf2687e8ed91f9cbf966d954d846e
bluepp <bluepp@gmail.com>
2017-03-08 14:52:22 +0800

    Update PalindromePartitioning.h

--- PalindromePartitioning.h
+++ PalindromePartitioning.h
@@ -20,57 +20,46 @@
     Solution: ...
  */
  
- bool isPal(string s)
-    {
-        int n = s.size();
-        int start = 0, end = n-1;
-        while (start <= end)
-        {
-            if (s[start] != s[end])
-            {
-                return false;
-            }
-           
-             start ++;
-             end --;
-          
-        }
-        
-        return true;
-     
-    }
-    
+
+/* 2017-03-08, update */
+
     vector<vector<string>> partition(string s) {
+        
+        vector<vector<string>> res;
         vector<string> vec;
-        vector<vector<string> > res;
-        int n = s.size();
         
-        _pal(s, 0, n, vec, res);
-        
+        _dfs(s, 0, vec, res);
         return res;
     }
     
-    void _pal(string s, int start, int end, vector<string> & vec, vector<vector<string> > &res)
+    void _dfs(string s, int start, vector<string> &vec, vector<vector<string>> &res)
     {
-        if (start >= end)
+        if (start == s.length())
         {
             res.push_back(vec);
             return;
         }
         
-        string tmp;
-        
-        for (int i = start; i < end; i++)
+        for (int i = start; i < s.length(); i++)
         {
-            tmp.push_back(s[i]);
-            if (!isPal(tmp))
-            {
-                continue;
-            }
-            vec.push_back(tmp);
-            _pal(s, i+1, end, vec, res);
+            string t = s.substr(start, i-start+1);
+            if (!isvalid(t)) continue;
+            
+            vec.push_back(t);
+            _dfs(s, i+1, vec, res);
             vec.pop_back();
         }
-     
+    }
+    
+    bool isvalid(string s)
+    {
+        int l = 0, r = s.length()-1;
+        
+        while (l < r)
+        {
+            if (s[l++] != s[r--]) return false;
+        }
+        
+        return true;
     }
  
