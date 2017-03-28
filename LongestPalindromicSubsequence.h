19cb8d8e9b9c120bff7bd5f109e125b3ce497ddd
bluepp <bluepp@gmail.com>
2017-03-28 14:00:32 +0800

    Create LongestPalindromicSubsequence.h

---
+++ LongestPalindromicSubsequence.h
@@ -0,0 +1,46 @@
+/*
+bluepp
+2017-03-28
+May the force be with me!
+
+https://leetcode.com/problems/longest-palindromic-subsequence/#/description
+
+Given a string s, find the longest palindromic subsequence's length in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+Input:
+
+"bbbab"
+Output:
+4
+One possible longest palindromic subsequence is "bbbb".
+Example 2:
+Input:
+
+"cbbd"
+Output:
+2
+One possible longest palindromic subsequence is "bb".
+Show Company Tags
+Show Tags
+Show Similar Problems
+
+*/
+
+    int longestPalindromeSubseq(string s) {
+        
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; ++j) {
+                if (s[i] == s[j]) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+        
+    }
