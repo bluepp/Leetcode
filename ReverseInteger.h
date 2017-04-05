a322d3cbbdfb66f27ef9273a6868d4a12a08f6c9
bluepp <bluepp@gmail.com>
2017-04-05 12:05:40 +0800

    Update ReverseInteger.h

--- ReverseInteger.h
+++ ReverseInteger.h
@@ -31,18 +31,20 @@
     On the other hand, we do not need to check if ret == 214748364, why?
  */
  
- public int reverse(int x) {
-   int ret = 0;
-   while (x != 0) {
-      // handle overflow/underflow
-      if (Math.abs(ret) > 214748364) {
-         return 0;
-      }
-      ret = ret * 10 + x % 10;
-      x /= 10;
-   }
-   return ret;
-}
+    int reverse(int x) {
+        
+        int ret = 0;
+        while (x != 0) {
+        // handle overflow/underflow
+            if (abs(ret) > 214748364) {
+                return 0;
+            }
+            ret = ret * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return ret;
+    }
  
  
  
