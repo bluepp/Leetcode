2c8f1e288b82a4979985238a3fb2a75b277a9c8b
bluepp <bluepp@gmail.com>
2018-10-27 00:38:30 +0800

    Update ReverseLinkedList.h

--- ReverseLinkedList.h
+++ ReverseLinkedList.h
@@ -24,6 +24,19 @@
         return prev;
         
     }
+
+/* 2018/10/26 */
+    ListNode* reverseList(ListNode* head) {
+        
+        if (!head || !head->next) return head;
+        
+        ListNode *p = head;
+        head = reverseList(p->next);
+        p->next->next = p;
+        p->next = NULL;
+        
+        return head;
+    }
     
     
     /* recursion */
