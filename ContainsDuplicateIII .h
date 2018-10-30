166bd115b0a4b80e1f866afa9d4bf0771705ee07
bluepp <bluepp@gmail.com>
2018-10-30 16:16:29 +0800

    Update ContainsDuplicateIII .h

--- ContainsDuplicateIII .h
+++ ContainsDuplicateIII .h
@@ -10,6 +10,22 @@
   https://leetcode.com/problems/contains-duplicate-iii/
 */
 
+
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        map<long long, int> m;
+        int j = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i - j > k) m.erase(nums[j++]);
+            auto a = m.lower_bound((long long)nums[i] - t);
+            if (a != m.end() && abs(a->first - nums[i]) <= t) return true;
+            m[nums[i]] = i;
+        }
+        
+        return false;
+      
+    }
+
+
 /* 2016-08-25, update */
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
         
