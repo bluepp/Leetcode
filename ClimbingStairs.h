6ded8c3bce42136b2b1ba63236907665ce756539
bluepp <bluepp@gmail.com>
2018-11-01 01:33:09 +0800

    Update ClimbingStairs.h

--- ClimbingStairs.h
+++ ClimbingStairs.h
@@ -14,6 +14,28 @@
 
     Solution: Clime one step from last stair or clime 2 steps from the last last stair.
  */
+
+
+/* 2019/11/01 */
+
+    int climbStairs(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
+        vector<int> dp(n, 0);
+        dp[0] = 1;
+        dp[1] = 2;
+        
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n-1];
+    }
+
+
+
+--------------------
  
     int climbStairs(int n) {
         int steps[n+1];
