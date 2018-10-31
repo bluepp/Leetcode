db8358ef9a9dbaf61f6cd5d52f9d69df801aaede
bluepp <bluepp@gmail.com>
2018-11-01 00:36:23 +0800

    Update RotateImage.h

--- RotateImage.h
+++ RotateImage.h
@@ -23,6 +23,25 @@
               789       369        963
  */
  
+/* 2018/10/31 8/
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        
+        for (int i = 0; i < n/2; i++) {
+            for (int j = 0; j < n; j++) {
+                swap(matrix[i][j], matrix[n-i-1][j]);
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+    }
+
+
  
   void rotate(vector<vector<int> > &matrix) {
         if (matrix.empty() || matrix[0].empty())
