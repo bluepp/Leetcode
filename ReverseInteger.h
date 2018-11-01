f59dc5547c1b91f89bd769cccda78d70798f876e
bluepp <bluepp@gmail.com>
2018-11-01 22:27:16 +0800

    Update ReverseInteger.h

--- ReverseInteger.h
+++ ReverseInteger.h
@@ -24,6 +24,25 @@
 
  Solution: Use % and / iteratively.
  */
+
+
+    int reverse(int x) {
+        long long ret = 0;
+        
+        while (x) {
+            ret = ret *10 + (x%10);
+            x /= 10;
+        }
+        
+        if (ret > INT_MAX || ret < INT_MIN) {
+            return 0;
+        } else {
+            return ret;
+        }
+
+    }
+
+
  
  /*
     2014-11-23
