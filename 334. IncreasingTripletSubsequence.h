eca46a045e92e547024a4db1f7c25a0e689313bf
bluepp <bluepp@gmail.com>
2018-11-09 16:59:16 +0800

    Update 334. IncreasingTripletSubsequence.h

--- 334. IncreasingTripletSubsequence.h
+++ 334. IncreasingTripletSubsequence.h
@@ -33,3 +33,30 @@ Output: false
         
         return false; 
     }
+
+
+/* ---------------------------------------------------- */
+    bool increasingTriplet(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 3) {
+            return false;
+        }
+        
+        vector<int> f(n, nums[0]), b(n, nums.back());
+        
+        for (int i = 1; i < n; i++) {
+            f[i] = min(f[i-1], nums[i]);
+        }
+        
+        for (int i = n-2; i >= 0; i--) {
+            b[i] = max(b[i+1], nums[i]);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > f[i] && nums[i] < b[i]) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
