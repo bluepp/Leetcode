836d2ed2a251222c58332cbce4b9622f3c7eb9a8
bluepp <bluepp@gmail.com>
2018-11-13 02:29:38 +0800

    Update PopulatingNextRightPointersinEachNodeII.h

--- PopulatingNextRightPointersinEachNodeII.h
+++ PopulatingNextRightPointersinEachNodeII.h
@@ -34,7 +34,35 @@
            2. iterative way + queue. Contributed by SUN Mian(孙冕).
            3. tail recursive solution.
  */
- 
+
+
+    void connect(TreeLinkNode *root) {
+        TreeLinkNode *dummy = new TreeLinkNode(0), *t = dummy;
+        
+        while (root) {
+            if (root->left) {
+                t->next = root->left;
+                t = t->next;
+            }
+            
+            if (root->right) {
+                t->next = root->right;
+                t = t->next;
+            }
+            
+            root = root->next;
+            
+            if (!root) {
+                t = dummy;
+                root = dummy->next;
+                dummy->next = NULL;
+            }
+        }
+    }
+
+
+
+/* --------------------------------------------------- */ 
 
 /* my version */
     void connect(TreeLinkNode *root) {
