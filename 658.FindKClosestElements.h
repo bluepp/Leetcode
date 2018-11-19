0103e3e3d2bc407be663213fcb50905fa675e0ff
bluepp <bluepp@gmail.com>
2018-11-19 16:38:47 +0800

    Create 658.FindKClosestElements.h

---
+++ 658.FindKClosestElements.h
@@ -0,0 +1,25 @@
+/*
+Given a sorted array, two integers k and x, find the k closest elements to x in the array. The result should also be sorted in ascending order. If there is a tie, the smaller elements are always preferred.
+
+Example 1:
+Input: [1,2,3,4,5], k=4, x=3
+Output: [1,2,3,4]
+Example 2:
+Input: [1,2,3,4,5], k=4, x=-1
+Output: [1,2,3,4]
+Note:
+The value k is positive and will always be smaller than the length of the sorted array.
+Length of the given array is positive and will not exceed 104
+Absolute value of elements in the array and x will not exceed 104
+
+*/
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left = 0, right = arr.size() - k;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (x - arr[mid] > arr[mid + k] - x) left = mid + 1;
+            else right = mid;
+        }
+        return vector<int>(arr.begin() + left, arr.begin() + left + k);
+    }
