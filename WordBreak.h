ad3209e78c73eeda1e6a7c1a68119db6b58eabf8
bluepp <bluepp@gmail.com>
2018-11-19 19:55:13 +0800

    Update WordBreak.h

--- WordBreak.h
+++ WordBreak.h
@@ -22,6 +22,34 @@
  Solution: dp.
 */
 
+
+/* memo */
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        vector<int> memo(s.size(), -1);
+        return _check(s, wordSet, 0, memo);
+    }
+    
+    bool _check(string s, unordered_set<string> &wordSet, int start, vector<int>& memo) {
+        if (start >= s.size()) {
+            return true;
+        }
+        
+        if (memo[start] != -1) {
+            return memo[start];
+        }
+        
+        for (int i = start+1; i <= s.length(); i++) {
+            if (wordSet.count(s.substr(start, i-start)) && _check(s, wordSet, i, memo)) {
+                return memo[start] = 1;
+            }
+        }
+        
+        return memo[start] = 0;
+    }
+
+
     bool wordBreak(string s, unordered_set<string> &dict) {
         int n = s.size();
         bool dp[n+1];
