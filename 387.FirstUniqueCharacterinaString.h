d383a52d12c9e52c1b76fab31e49c1e2e7ab9eb6
bluepp <bluepp@gmail.com>
2018-12-18 18:50:37 +0800

    Update and rename FirstUniqueCharacterinaString.h to 387.FirstUniqueCharacterinaString.h

--- 387.FirstUniqueCharacterinaString.h
+++ 387.FirstUniqueCharacterinaString.h
@@ -18,22 +18,19 @@ Note: You may assume the string contain only lowercase letters.
 */
 
     int firstUniqChar(string s) {
+        int ret = -1;
         
         unordered_map<char, int> map;
-        
-        for (auto p : s)
-        {
+        for (auto p : s) {
             map[p]++;
         }
         
-        for (int i = 0; i < s.length(); i++)
-        {
-            if (map[s[i]] == 1)
-            {
-                return i;
+        for (int i = 0; i < s.length(); i++) {
+            if (map[s[i]] == 1) {
+                ret = i;
+                break;
             }
         }
         
-        return -1;
-     
+        return ret;
     }
