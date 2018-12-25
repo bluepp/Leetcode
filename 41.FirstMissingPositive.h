ba31fb3416d29510c1dd58f77338ca41f7125e5e
bluepp <bluepp@gmail.com>
2018-12-25 15:48:16 +0800

    Update and rename FirstMissingPositive.h to 41.FirstMissingPositive.h

--- 41.FirstMissingPositive.h
+++ 41.FirstMissingPositive.h
@@ -23,6 +23,29 @@
            Pick out the first one that does not satisfy A[i] == i + 1.
  */
  
+
+
+    int firstMissingPositive(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] < n && nums[i] != nums[nums[i]-1]) {
+                swap(nums[i], nums[nums[i]-1]);
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i+1) {
+                return i+1;
+            }
+        }
+        
+        return n+1;   
+    }
+
+
+
 /* 2016-06-17, update */
     int firstMissingPositive(vector<int>& nums) {
         
