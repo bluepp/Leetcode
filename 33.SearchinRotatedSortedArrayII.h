a21e2b665a29caf37be461b0b316ce3f69ad1a10
bluepp <bluepp@gmail.com>
2019-01-03 01:21:01 +0800

    Update and rename SearchinRotatedSortedArrayII.h to 33.SearchinRotatedSortedArrayII.h

--- 33.SearchinRotatedSortedArrayII.h
+++ 33.SearchinRotatedSortedArrayII.h
@@ -19,6 +19,39 @@
            Since there are duplicates, it's hard to decide which branch to go if binary-search is deployed.
  */
  
+
+
+
+    int search(vector<int>& nums, int target) {
+        
+        int l = 0, r = nums.size()-1;
+        
+        while (l <= r) {
+            int m = l + (r-l)/2;
+            
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] < nums[r]) {
+                if (target > nums[m] && target <= nums[r]) {
+                    l = m+1;
+                } else {
+                    r = m-1;
+                }
+            } else {
+                if (target >= nums[l] && target < nums[m]) {
+                    r = m-1;
+                } else {
+                    l = m+1;
+                }
+            }
+            
+        }
+        
+      
+        return -1;
+    }
+
+
  /* 2016-07-18, update binary search */
      bool search(vector<int>& nums, int target) {
         
