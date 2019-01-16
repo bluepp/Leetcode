4ab430f5ca05c910b1d4fe2c21184ed0ae77ac2b
bluepp <bluepp@gmail.com>
2019-01-16 19:35:08 +0800

    Update and rename MinimumWindowSubstring.h to 76.MinimumWindowSubstring.h

--- 76.MinimumWindowSubstring.h
+++ 76.MinimumWindowSubstring.h
@@ -30,6 +30,41 @@
               First, move 'end'. After finding all the needed characters, move 'start'.
            2. Use array as hashtable.
  */
+
+
+    string minWindow(string s, string t) {
+        string res;
+        unordered_map<char, int> map;
+        
+        for (auto p : t) {
+            map[p]++;
+        }
+        
+        int left = 0, count = 0, minLen = INT_MAX;
+        
+        for(int i = 0; i < s.size(); i++) {
+            if (--map[s[i]] >= 0) {
+                count ++;
+            }
+            
+            while (count == t.size()) {
+                if (minLen > i - left + 1) {
+                    minLen = i-left+1;
+                    res = s.substr(left, minLen);
+                }
+                
+                if (++map[s[left]] > 0) {
+                    count--;
+                }
+                
+                left ++;
+            }
+        }
+        
+        return res;
+        
+    }
+
  
 /* 2016-12-31, update */
     string minWindow(string s, string t) {
