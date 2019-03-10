c21e9524863fa5897f45b5d98fa8d115efc76726
bluepp <bluepp@gmail.com>
2019-03-10 23:34:28 +0800

    Update 894.AllPossibleFullBinaryTrees.h

--- 894.AllPossibleFullBinaryTrees.h
+++ 894.AllPossibleFullBinaryTrees.h
@@ -15,8 +15,47 @@ Input: 7
 Output: [[0,0,0,null,null,0,0,null,null,0,0],[0,0,0,null,null,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,null,null,null,null,0,0],[0,0,0,0,0,null,null,0,0]]
 Explanation:
 
- 
  */   
+
+
+    vector<TreeNode*> allPossibleFBT(int N) {
+        
+        vector<TreeNode *> res;
+        if (N == 0) {
+            return res;
+        }
+        
+        if (N == 1) {
+            TreeNode *root = new TreeNode(0);
+            res.push_back(root);
+            return res;
+        }
+        
+        if (N % 2 == 0){
+            return res;
+        }
+        
+        for (int i = 1; i < N; i += 2){
+            vector<TreeNode *> lvec = allPossibleFBT(i);
+            vector<TreeNode *> rvec = allPossibleFBT(N-1-i);
+            
+            for (int j = 0; j < lvec.size(); j++){
+                for (int k = 0; k < rvec.size(); k++) {
+                    TreeNode *root = new TreeNode(0);
+                    root->left = lvec[j];
+                    root->right = rvec[k];
+                    
+                    res.push_back(root);
+                }
+            }
+        }
+        
+        return res;
+    }
+
+
+
+
  
  vector<TreeNode*> allPossibleFBT(int N) {
         if (N % 2 == 0) return {};
