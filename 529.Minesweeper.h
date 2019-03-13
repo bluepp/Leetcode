4d8ede36be82cefc6cbca5a52ada514dbd564759
bluepp <bluepp@gmail.com>
2019-03-13 16:26:35 +0800

    Update 529.Minesweeper.h

--- 529.Minesweeper.h
+++ 529.Minesweeper.h
@@ -62,6 +62,52 @@ For simplicity, not mentioned rules should be ignored in this problem. For examp
 */
 
 
+    vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click){
+        
+        if (board.empty() || board[0].empty()){
+            return {};
+        }
+        
+        int m = board.size(), n = board[0].size();
+        int row = click[0], col = click[1];
+        int count = 0;
+      
+        if (board[row][col] == 'M'){
+            board[row][col] = 'X';
+        } else {
+            
+            vector<vector<int>> neighbors;
+            
+            for (int i = -1; i < 2; i++){
+                for (int j = -1; j < 2; j++){
+                    int x = row+i, y = col+j;
+                    
+                    if (x < 0 || x >= m || y < 0 || y >= n){
+                        continue;
+                    }
+                    
+                    if (board[x][y] == 'M'){
+                        count++;
+                    } else if (count == 0 && board[x][y] == 'E'){
+                        neighbors.push_back({x, y});
+                    }
+                }
+            }
+            
+            if (count > 0){
+                board[row][col] = count+'0';
+            } else {
+                for (auto p : neighbors){
+                    board[p[0]][p[1]] = 'B';
+                    updateBoard(board, p);
+                }
+            }
+        }
+      
+        return board;
+    }
+
+
 
 /* wrong... */
     vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click){
